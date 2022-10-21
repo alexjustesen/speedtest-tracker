77bab4e8f5d29fee245b2097950fa7ef7e9bb0f5
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-10-21 06:12:32 -0400

    new dashboard page with adhoc speedtest button

---
+++ resources/views/filament/pages/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-filament::page>
+    <p>
+        👋 Welcome to Speedtest Tracker, here you can track your internet speed using Ookla's Speedtest service. Keep in mind that this is still work in progress so make sure to check out the documentation and follow along on GitHub for updates.
+    </p>
+</x-filament::page>
