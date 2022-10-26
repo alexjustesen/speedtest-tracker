3ef6236604415644dca8101016149eb2330da21c
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-10-25 21:24:05 -0400

    added absoluteThresholdPassed helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,14 +1,37 @@
 <?php
 
+use App\Models\Result;
+use App\Settings\ThresholdSettings;
+
+if (! function_exists('absoluteThresholdPassed')) {
+    function absoluteThresholdPassed(Result $result): bool
+    {
+        $thresholds = new (ThresholdSettings::class);
+
+        if (! $thresholds->absolute_enabled) {
+            return true;
+        }
+
+        return formatBits(formatBytesToBits($result->download), 2, false) > $thresholds->absolute_download
+            && formatBits(formatBytesToBits($result->upload), 2, false) > $thresholds->absolute_upload
+            && $result->ping < $thresholds->absolute_ping;
+    }
+}
+
 if (! function_exists('formatBits')) {
-    function formatBits(int $bits, $precision = 2)
+    function formatBits(int $bits, $precision = 2, $suffix = true)
     {
         if ($bits > 0) {
             $i = floor(log($bits) / log(1000));
 
+            if (! $suffix) {
+                return round($bits / pow(1000, $i), $precision);
+            }
+
             $sizes = ['B', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb', 'Zb', 'Yb'];
 
             return sprintf('%.02F', round($bits / pow(1000, $i), $precision)) * 1 .' '.@$sizes[$i];
+
         }
 
         return 0;
