00f3be12b7eeb6ce71465a747f702e3cb5b42b3e
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-10-28 07:26:57 -0400

    Failed jobs viewer (#94)

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Amvisor\FilamentFailedJobs\Models\FailedJob;
+use App\Policies\FailedJobPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -13,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        FailedJob::class => FailedJobPolicy::class,
     ];
 
     /**
