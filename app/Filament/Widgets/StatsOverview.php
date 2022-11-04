70d5a800f5a9d6cc8a76ac244c331cc106afb90e
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-11-04 07:38:20 -0400

    Automatically refresh dashboard (#124)

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -8,8 +8,6 @@ use Filament\Widgets\StatsOverviewWidget\Card;
 
 class StatsOverview extends BaseWidget
 {
-    protected static ?string $pollingInterval = null;
-
     protected function getCards(): array
     {
         $result = Result::latest()->first();
