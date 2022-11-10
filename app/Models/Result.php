2bb13fa93696d8c699738e94b568532b11d70f4a
adamus1red <adamus1red@users.noreply.github.com>
2022-11-10 19:13:59 +0000

    Add jitter data to InfluxDB feed (#136)

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -40,6 +40,7 @@ class Result extends Model
      */
     protected $casts = [
         'scheduled' => 'boolean',
+        'data' => 'array',
         'created_at' => 'datetime',
     ];
 
@@ -53,10 +54,16 @@ class Result extends Model
             'ping' => (float) $this->ping,
             'download' => (int) $this->download,
             'upload' => (int) $this->upload,
+            'download_bits' => (int) $this->download * 8,
+            'upload_bits' => (int) $this->upload * 8,
+            'ping_jitter' => (float) $this->data['ping']['jitter'],
+            'download_jitter' => (float) $this->data['download']['latency']['jitter'],
+            'upload_jitter' => (float) $this->data['upload']['latency']['jitter'],
             'server_id' => (int) $this->server_id,
             'server_host' => $this->server_host,
             'server_name' => $this->server_name,
             'scheduled' => $this->scheduled,
+            'packet_loss' => $this->data['packetLoss'],
         ];
     }
 }
