9684f516cdc7349a54cf140ec87a1d966d46d9fb
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-11-11 09:54:48 -0500

    mail notification settings

--- app/Settings/NotificationSettings.php
+++ app/Settings/NotificationSettings.php
@@ -12,6 +12,14 @@ class NotificationSettings extends Settings
 
     public bool $database_on_threshold_failure;
 
+    public bool $mail_enabled;
+
+    public bool $mail_on_speedtest_run;
+
+    public bool $mail_on_threshold_failure;
+
+    public ?string $mail_recipients;
+
     public static function group(): string
     {
         return 'notification';
