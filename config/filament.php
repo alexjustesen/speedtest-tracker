ae8f7cf6c93d5d094f3e313bc0f946ad314fea0e
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-11-26 07:22:29 -0500

    enabled collapsible sidebar

--- config/filament.php
+++ config/filament.php
@@ -244,7 +244,7 @@ return [
             'alignment' => 'right',
         ],
         'sidebar' => [
-            'is_collapsible_on_desktop' => false,
+            'is_collapsible_on_desktop' => true,
             'groups' => [
                 'are_collapsible' => true,
             ],
