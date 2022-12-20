94d3ab48be91da8f1eeb861180ebcc2e5d8485a5
Alex Justesen <alexjustesen@users.noreply.github.com>
2022-12-20 15:33:10 -0500

    Last run date polling (#252)

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,7 +1,5 @@
 <x-filament::page>
-
-<div>
-    <p class="text-center text-sm">Last speedtest run at: <strong>{{ $lastResult }}</strong></p>
-</div>
-
+    <div wire:poll.5000ms>
+        <p class="text-center text-sm">Last speedtest run at: <strong>{{ $lastResult }}</strong></p>
+    </div>
 </x-filament::page>
