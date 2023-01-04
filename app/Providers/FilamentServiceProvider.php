4e219be4c16e91b9923e685cebbfc0407ecedcca
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-01-03 21:00:00 -0500

    moved build information to speedtest config

--- app/Providers/FilamentServiceProvider.php
+++ app/Providers/FilamentServiceProvider.php
@@ -32,7 +32,7 @@ class FilamentServiceProvider extends ServiceProvider
             return true;
         });
 
-        FilamentVersions::addItem('Speedtest Tracker', 'v0.9.1');
+        FilamentVersions::addItem('Speedtest Tracker', 'v'.config('speedtest.build_version'));
 
         Filament::serving(function () {
             Filament::registerNavigationGroups([
---
+++ config/speedtest.php
@@ -0,0 +1,12 @@
+<?php
+
+use Carbon\Carbon;
+
+return [
+    /**
+     * Build information
+     */
+    'build_date' => Carbon::parse('2023-01-03'),
+
+    'build_version' => '0.9.2',
+];
