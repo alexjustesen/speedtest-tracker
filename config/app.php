ca0597920fc892b81823a1d5cbddcd69e60a8dc4
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-01-05 16:53:58 -0500

    added config to force https scheme

--- config/app.php
+++ config/app.php
@@ -58,6 +58,8 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'force_https' => env('FORCE_HTTPS', false),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
