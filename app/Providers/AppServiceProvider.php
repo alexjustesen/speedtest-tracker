c6ae6aad0f6dc87a3dd3e5d3b116e30ce8bd3161
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-01-05 16:56:41 -0500

    force https (#299)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.force_https')) {
+            URL::forceScheme('https');
+        }
     }
 }
