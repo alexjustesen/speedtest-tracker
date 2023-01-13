2aaa596fc12adcdc2ffce35b497dd281b95087a3
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-01-12 21:02:11 -0500

    delete speedtest results from the ui

---
+++ app/Jobs/DeleteResultsData.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Result;
+use App\Models\User;
+use Filament\Notifications\Notification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class DeleteResultsData implements ShouldQueue, ShouldBeUnique
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $count = Result::count();
+
+        try {
+            DB::table('results')->truncate();
+        } catch (\Throwable $th) {
+            //throw $th;
+
+            return 0;
+        }
+
+        $recipient = User::first();
+
+        Notification::make()
+            ->title('Speedtest results deleted')
+            ->body($count.' speedtest results were deleted from the database.')
+            ->success()
+            ->sendToDatabase($recipient);
+    }
+}
