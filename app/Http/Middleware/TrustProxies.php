740d5f89c14fe95b0bd986c5e9c8a0f8b5303246
Alessandro Del Prete <alessandro.delprete@gmail.com>
2023-01-20 06:04:32 +0100

    Unrolled TRUSTED_PROXIES

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = env('TRUSTED_PROXIES', '*');
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
