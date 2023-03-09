dc456ff431424adc8b761fb75964f3827dfc559b
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2023-03-09 18:21:35 -0500

    Homepage API support (#463)
    Co-authored-by: Alex Justesen <alexjustesen@users.noreply.github.com>

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\Speedtest\GetLatestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/speedtest/latest', GetLatestController::class)
+    ->name('speedtest.latest');
