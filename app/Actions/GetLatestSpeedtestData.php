38698d7b57d4747f68dbb9986a0ab9fb955c08d2
cinderblockgames <79210192+cinderblockgames@users.noreply.github.com>
2023-03-08 22:48:28 -0500

    Removing extra data.

--- app/Actions/GetLatestSpeedtestData.php
+++ app/Actions/GetLatestSpeedtestData.php
@@ -2,7 +2,6 @@
 
 namespace App\Actions;
 
-use App\Helpers\SettingsHelper;
 use App\Helpers\SpeedtestHelper;
 use App\Models\Speedtest;
 use DB;
@@ -22,30 +21,6 @@ class GetLatestSpeedtestData implements ActionInterface
         $response = [
             'data' => $data,
         ];
-
-        if (SettingsHelper::get('show_average')) {
-            $avg = Speedtest::select(DB::raw('AVG(ping) as ping, AVG(download) as download, AVG(upload) as upload'))
-                ->where('failed', false)
-                ->first()
-                ->toArray();
-            $response['average'] = $avg;
-        }
-
-        if (SettingsHelper::get('show_max')) {
-            $max = Speedtest::select(DB::raw('MAX(ping) as ping, MAX(download) as download, MAX(upload) as upload'))
-                ->where('failed', false)
-                ->first()
-                ->toArray();
-            $response['maximum'] = $max;
-        }
-
-        if (SettingsHelper::get('show_min')) {
-            $min = Speedtest::select(DB::raw('MIN(ping) as ping, MIN(download) as download, MIN(upload) as upload'))
-                ->where('failed', false)
-                ->first()
-                ->toArray();
-            $response['minimum'] = $min;
-        }
 
         return $response;
     }
