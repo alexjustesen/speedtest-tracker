c252d332f06f0e2249d9e423ee4fbf40984da29a
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-04-18 20:00:49 -0400

    removed unused policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,10 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
-// TODO: remove package and policy as FailedJob is no longer included
-use Amvisor\FilamentFailedJobs\Models\FailedJob;
 use App\Models\Result;
-use App\Policies\FailedJobPolicy;
 use App\Policies\ResultPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -19,7 +16,6 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        FailedJob::class => FailedJobPolicy::class,
         Result::class => ResultPolicy::class,
     ];
 
