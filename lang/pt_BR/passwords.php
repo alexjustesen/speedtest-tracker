83a1bd608c530d1e6ff3482b4bce9afa9cb37212
Carlos Albuquerque Carneiro <carloscarneiropmw@gmail.com>
2023-05-06 15:10:08 -0300

    formatting fix

--- lang/pt_BR/passwords.php
+++ lang/pt_BR/passwords.php
@@ -18,6 +18,6 @@ return [
     'sent' => 'Enviamos seu link de redefinição de senha por e-mail!',
     'throttled' => 'Aguarde antes de tentar novamente.',
     'token' => 'Este token de redefinição de senha é inválido.',
-    'user' => "Não encontramos um usuário com esse endereço de e-mail.",
+    'user' => 'Não encontramos um usuário com esse endereço de e-mail.',
 
 ];
