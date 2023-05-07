7b9a3b01362ec171f334a58d1f7e6b199d105353
Shift <shift@laravelshift.com>
2023-05-07 11:51:11 +0000

    Rename `password_resets` table

--- config/auth.php
+++ config/auth.php
@@ -93,7 +93,7 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_resets',
+            'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
         ],
