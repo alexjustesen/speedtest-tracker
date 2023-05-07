efa1dd91f3294c46777e91c6673a92f0356f919b
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-05-07 12:26:43 -0400

    CI Generator (#578)
    Co-authored-by: Shift <shift@laravelshift.com>

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+final class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
