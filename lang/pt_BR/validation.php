ae3a0bfeb581503921180e88ae897ca8aadc90c2
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-05-07 07:46:02 -0400

    too many spaces

--- lang/pt_BR/validation.php
+++ lang/pt_BR/validation.php
@@ -91,7 +91,7 @@ return [
     ],
     'max_digits' => 'O campo :attribute não pode ser superior a :max dígitos',
     'mimes' => 'O campo :attribute deve ser um arquivo do tipo: :values.',
-    'mimetypes'  => 'O campo :attribute deve ser um arquivo do tipo: :values.',
+    'mimetypes' => 'O campo :attribute deve ser um arquivo do tipo: :values.',
     'min' => [
         'numeric' => 'O campo :attribute deve ser pelo menos :min.',
         'file' => 'O campo :attribute deve ter pelo menos :min kilobytes.',
