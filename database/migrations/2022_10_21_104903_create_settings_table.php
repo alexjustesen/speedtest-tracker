ac5ef6aa4d5845dfc685acb49e2b35de78d2c464
Shift <shift@laravelshift.com>
2023-05-07 11:51:11 +0000

    Adopt anonymous migrations

--- database/migrations/2022_10_21_104903_create_settings_table.php
+++ database/migrations/2022_10_21_104903_create_settings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSettingsTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -24,4 +24,4 @@ class CreateSettingsTable extends Migration
     {
         Schema::dropIfExists('settings');
     }
-}
+};
