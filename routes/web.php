ab0138b20a26c503694a731004a7dfc0190fd0c7
Shift <shift@laravelshift.com>
2023-05-10 17:45:53 +0000

    Convert route array to fluent methods

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect()->to('/admin');
-});
+Route::redirect('/', '/admin');
 
 if (app()->isLocal()) {
     require __DIR__.'/test.php';
