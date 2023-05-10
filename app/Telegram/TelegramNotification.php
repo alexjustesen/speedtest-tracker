54c0615d8bcda3507751d93b198ee8db5f24ec75
Shift <shift@laravelshift.com>
2023-05-10 17:45:56 +0000

    Remove redundant typing from DocBlocks

--- app/Telegram/TelegramNotification.php
+++ app/Telegram/TelegramNotification.php
@@ -27,7 +27,6 @@ class TelegramNotification extends Notification
      * Get the notification's delivery channels.
      *
      * @param  mixed  $notifiable
-     * @return array
      */
     public function via($notifiable): array
     {
@@ -38,7 +37,6 @@ class TelegramNotification extends Notification
      * Get the mail representation of the notification.
      *
      * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toTelegram($notifiable): MailMessage
     {
