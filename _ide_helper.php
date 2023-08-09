b0b725be7ed943e2a12cfd8abb13d731df8e0ca4
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-08-09 18:47:25 -0400

    updated composer dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -4,7 +4,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.17.0.
+ * Generated for Laravel 10.18.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
@@ -18249,6 +18249,20 @@
         {
                         /** @var \Illuminate\Foundation\Vite $instance */
                         return $instance->asset($asset, $buildDirectory);
+        }
+                    /**
+         * Get the content of a given asset.
+         *
+         * @param string $asset
+         * @param string|null $buildDirectory
+         * @return string 
+         * @throws \Exception
+         * @static 
+         */ 
+        public static function content($asset, $buildDirectory = null)
+        {
+                        /** @var \Illuminate\Foundation\Vite $instance */
+                        return $instance->content($asset, $buildDirectory);
         }
                     /**
          * Get a unique hash representing the current manifest, or null if there is no manifest.
