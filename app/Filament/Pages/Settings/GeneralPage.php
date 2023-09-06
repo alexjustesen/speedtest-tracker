1d8e750ef0aa06488e1ce142e897e589379c27fb
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-09-05 23:06:22 -0400

    updated settings pages

--- app/Filament/Pages/Settings/GeneralPage.php
+++ app/Filament/Pages/Settings/GeneralPage.php
@@ -4,10 +4,8 @@ namespace App\Filament\Pages\Settings;
 
 use App\Rules\Cron;
 use App\Settings\GeneralSettings;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
+use Filament\Forms;
+use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
 use Illuminate\Support\Facades\Http;
 use Squire\Models\Timezone;
@@ -26,134 +24,127 @@ class GeneralPage extends SettingsPage
 
     protected static string $settings = GeneralSettings::class;
 
-    protected function getMaxContentWidth(): string
+    public function getMaxContentWidth(): string
     {
         $settings = new GeneralSettings();
 
         return $settings->content_width;
     }
 
-    protected function getFormSchema(): array
+    public function form(Form $form): Form
     {
-        return [
-            Grid::make([
-                'default' => 1,
-                'md' => 3,
-            ])
-                ->schema([
-                    Grid::make([
-                        'default' => 1,
+        return $form
+            ->schema([
+                Forms\Components\Grid::make([
+                    'default' => 1,
+                ])
+                    ->schema([
+                        Forms\Components\Section::make('Site Settings')
+                            ->schema([
+                                Forms\Components\TextInput::make('site_name')
+                                    ->maxLength(50)
+                                    ->required()
+                                    ->columnSpan(['md' => 2]),
+                                Forms\Components\Select::make('timezone')
+                                    ->options(Timezone::all()->pluck('code', 'code'))
+                                    ->searchable()
+                                    ->required(),
+                                Forms\Components\TextInput::make('time_format')
+                                    ->helperText('Use [DateTime Format](https://www.php.net/manual/en/datetime.format.php) options to change the format of the datetime in views.')
+                                    ->placeholder('M j, Y G:i:s')
+                                    ->maxLength(25)
+                                    ->required(),
+                                Forms\Components\Select::make('content_width')
+                                    ->options([
+                                        '7xl' => 'Default width',
+                                        'full' => 'Full width',
+                                    ]),
+                            ])
+                            ->compact()
+                            ->columns([
+                                'default' => 1,
+                                'md' => 2,
+                            ]),
+
+                        Forms\Components\Section::make('Speedtest Settings')
+                            ->schema([
+                                Forms\Components\TextInput::make('speedtest_schedule')
+                                    ->rules([new Cron()])
+                                    ->helperText('Leave empty to disable the schedule. You can also use the cron expression generator [HERE](https://crontab.cronhub.io/) to help you make schedules.')
+                                    ->nullable()
+                                    ->columnSpan(1),
+                                Forms\Components\Select::make('speedtest_server')
+                                    ->label('Speedtest server ID')
+                                    ->helperText('Leave empty to let the system pick the best server.')
+                                    ->nullable()
+                                    ->multiple()
+                                    ->maxItems(10)
+                                    ->searchable()
+                                    ->options(function () {
+                                        $response = Http::get(
+                                            url: 'https://www.speedtest.net/api/js/servers',
+                                            query: [
+                                                'engine' => 'js',
+                                                'https_functional' => true,
+                                                'limit' => 10,
+                                            ]
+                                        );
+
+                                        if ($response->failed()) {
+                                            return [
+                                                '' => 'There was an error retrieving Speedtest servers',
+                                            ];
+                                        }
+
+                                        return $response->collect()->map(function ($item) {
+                                            return [
+                                                'id' => $item['id'],
+                                                'name' => $item['id'].': '.$item['name'].' ('.$item['sponsor'].')',
+                                            ];
+                                        })->pluck('name', 'id');
+                                    })
+                                    ->getSearchResultsUsing(function (string $search) {
+                                        $response = Http::get(
+                                            url: 'https://www.speedtest.net/api/js/servers',
+                                            query: [
+                                                'engine' => 'js',
+                                                'search' => $search,
+                                                'https_functional' => true,
+                                                'limit' => 10,
+                                            ]
+                                        );
+
+                                        if ($response->failed()) {
+                                            return [
+                                                '' => 'There was an error retrieving Speedtest servers',
+                                            ];
+                                        }
+
+                                        if (! $response->collect()->count() && is_numeric($search)) {
+                                            return collect([
+                                                [
+                                                    'id' => $search,
+                                                    'name' => $search.': No server found, manually add this ID.',
+                                                ],
+                                            ]);
+                                        }
+
+                                        return $response->collect()->map(function ($item) {
+                                            return [
+                                                'id' => $item['id'],
+                                                'name' => $item['id'].': '.$item['name'].' ('.$item['sponsor'].')',
+                                            ];
+                                        })->pluck('name', 'id');
+                                    })
+                                    ->columnSpan(2),
+                            ])
+                            ->compact()
+                            ->columns([
+                                'default' => 1,
+                                'md' => 2,
+                            ]),
                     ])
-                        ->schema([
-                            Section::make('Site Settings')
-                                ->schema([
-                                    TextInput::make('site_name')
-                                        ->maxLength(50)
-                                        ->required()
-                                        ->columnSpan(['md' => 2]),
-                                    Select::make('timezone')
-                                        ->options(Timezone::all()->pluck('code', 'code'))
-                                        ->searchable()
-                                        ->required(),
-                                    TextInput::make('time_format')
-                                        ->helperText('Use [DateTime Format](https://www.php.net/manual/en/datetime.format.php) options to change the format of the datetime in views.')
-                                        ->placeholder('M j, Y G:i:s')
-                                        ->maxLength(25)
-                                        ->required(),
-                                    Select::make('content_width')
-                                        ->options([
-                                            '7xl' => 'Default width',
-                                            'full' => 'Full width',
-                                        ]),
-                                ])
-                                ->compact()
-                                ->columns([
-                                    'default' => 1,
-                                    'md' => 2,
-                                ]),
-
-                            Section::make('Speedtest Settings')
-                                ->schema([
-                                    TextInput::make('speedtest_schedule')
-                                        ->rules([new Cron()])
-                                        ->helperText('Leave empty to disable the schedule. You can also use the cron expression generator [HERE](https://crontab.cronhub.io/) to help you make schedules.')
-                                        ->nullable()
-                                        ->columnSpan(1),
-                                    Select::make('speedtest_server')
-                                        ->label('Speedtest server ID')
-                                        ->helperText('Leave empty to let the system pick the best server.')
-                                        ->nullable()
-                                        ->multiple()
-                                        ->maxItems(10)
-                                        ->searchable()
-                                        ->options(function () {
-                                            $response = Http::get(
-                                                url: 'https://www.speedtest.net/api/js/servers',
-                                                query: [
-                                                    'engine' => 'js',
-                                                    'https_functional' => true,
-                                                    'limit' => 10,
-                                                ]
-                                            );
-
-                                            if ($response->failed()) {
-                                                return [
-                                                    '' => 'There was an error retrieving Speedtest servers',
-                                                ];
-                                            }
-
-                                            return $response->collect()->map(function ($item) {
-                                                return [
-                                                    'id' => $item['id'],
-                                                    'name' => $item['id'].': '.$item['name'].' ('.$item['sponsor'].')',
-                                                ];
-                                            })->pluck('name', 'id');
-                                        })
-                                        ->getSearchResultsUsing(function (string $search) {
-                                            $response = Http::get(
-                                                url: 'https://www.speedtest.net/api/js/servers',
-                                                query: [
-                                                    'engine' => 'js',
-                                                    'search' => $search,
-                                                    'https_functional' => true,
-                                                    'limit' => 10,
-                                                ]
-                                            );
-
-                                            if ($response->failed()) {
-                                                return [
-                                                    '' => 'There was an error retrieving Speedtest servers',
-                                                ];
-                                            }
-
-                                            if (! $response->collect()->count() && is_numeric($search)) {
-                                                return collect([
-                                                    [
-                                                        'id' => $search,
-                                                        'name' => $search.': No server found, manually add this ID.',
-                                                    ],
-                                                ]);
-                                            }
-
-                                            return $response->collect()->map(function ($item) {
-                                                return [
-                                                    'id' => $item['id'],
-                                                    'name' => $item['id'].': '.$item['name'].' ('.$item['sponsor'].')',
-                                                ];
-                                            })->pluck('name', 'id');
-                                        })
-                                        ->columnSpan(2),
-                                ])
-                                ->compact()
-                                ->columns([
-                                    'default' => 1,
-                                    'md' => 2,
-                                ]),
-                        ])
-                        ->columnSpan([
-                            'md' => 2,
-                        ]),
-                ]),
-        ];
+                    ->columnSpan('full'),
+            ]);
     }
 }
--- app/Filament/Pages/Settings/ThresholdsPage.php
+++ app/Filament/Pages/Settings/ThresholdsPage.php
@@ -4,19 +4,13 @@ namespace App\Filament\Pages\Settings;
 
 use App\Settings\GeneralSettings;
 use App\Settings\ThresholdSettings;
-use Closure;
-use Filament\Forms\Components\Card;
-use Filament\Forms\Components\Fieldset;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\View;
+use Filament\Forms;
+use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
 
 class ThresholdsPage extends SettingsPage
 {
-    protected static ?string $navigationIcon = 'heroicon-o-exclamation';
+    protected static ?string $navigationIcon = 'heroicon-o-exclamation-triangle';
 
     protected static ?string $navigationGroup = 'Settings';
 
@@ -28,88 +22,89 @@ class ThresholdsPage extends SettingsPage
 
     protected static string $settings = ThresholdSettings::class;
 
-    protected function getMaxContentWidth(): string
+    public function getMaxContentWidth(): string
     {
         $settings = new GeneralSettings();
 
         return $settings->content_width;
     }
 
-    protected function getFormSchema(): array
+    public function form(Form $form): Form
     {
-        return [
-            Grid::make([
-                'default' => 1,
-                'md' => 3,
-            ])
-                ->schema([
-                    Grid::make([
-                        'default' => 1,
-                    ])
-                        ->schema([
-                            Section::make('Absolute')
-                                ->description('Absolute thresholds do not take into account previous history and could be triggered on each test.')
-                                ->schema([
-                                    Toggle::make('absolute_enabled')
-                                        ->label('Enable absolute thresholds')
-                                        ->reactive()
-                                        ->columnSpan(2),
-                                    Grid::make([
-                                        'default' => 1,
+        return $form
+            ->schema([
+                Forms\Components\Grid::make([
+                    'default' => 1,
+                    'md' => 3,
+                ])
+                    ->schema([
+                        Forms\Components\Grid::make([
+                            'default' => 1,
+                        ])
+                            ->schema([
+                                Forms\Components\Section::make('Absolute')
+                                    ->description('Absolute thresholds do not take into account previous history and could be triggered on each test.')
+                                    ->schema([
+                                        Forms\Components\Toggle::make('absolute_enabled')
+                                            ->label('Enable absolute thresholds')
+                                            ->reactive()
+                                            ->columnSpan(2),
+                                        Forms\Components\Grid::make([
+                                            'default' => 1,
+                                        ])
+                                            ->hidden(fn (Forms\Get $get) => $get('absolute_enabled') !== true)
+                                            ->schema([
+                                                Forms\Components\Fieldset::make('Metrics')
+                                                    ->schema([
+                                                        Forms\Components\TextInput::make('absolute_download')
+                                                            ->label('Download')
+                                                            ->hint('Mbps')
+                                                            ->helperText('Set to zero to disable this metric.')
+                                                            ->default(0)
+                                                            ->minValue(0)
+                                                            ->numeric()
+                                                            ->required(),
+                                                        Forms\Components\TextInput::make('absolute_upload')
+                                                            ->label('Upload')
+                                                            ->hint('Mbps')
+                                                            ->helperText('Set to zero to disable this metric.')
+                                                            ->default(0)
+                                                            ->minValue(0)
+                                                            ->numeric()
+                                                            ->required(),
+                                                        Forms\Components\TextInput::make('absolute_ping')
+                                                            ->label('Ping')
+                                                            ->hint('Ms')
+                                                            ->helperText('Set to zero to disable this metric.')
+                                                            ->default(0)
+                                                            ->minValue(0)
+                                                            ->numeric()
+                                                            ->required(),
+                                                    ])
+                                                    ->columns([
+                                                        'default' => 1,
+                                                        'md' => 2,
+                                                    ]),
+                                            ]),
                                     ])
-                                        ->hidden(fn (Closure $get) => $get('absolute_enabled') !== true)
-                                        ->schema([
-                                            Fieldset::make('Metrics')
-                                                ->schema([
-                                                    TextInput::make('absolute_download')
-                                                        ->label('Download')
-                                                        ->hint('Mbps')
-                                                        ->helperText('Set to zero to disable this metric.')
-                                                        ->default(0)
-                                                        ->minValue(0)
-                                                        ->numeric()
-                                                        ->required(),
-                                                    TextInput::make('absolute_upload')
-                                                        ->label('Upload')
-                                                        ->hint('Mbps')
-                                                        ->helperText('Set to zero to disable this metric.')
-                                                        ->default(0)
-                                                        ->minValue(0)
-                                                        ->numeric()
-                                                        ->required(),
-                                                    TextInput::make('absolute_ping')
-                                                        ->label('Ping')
-                                                        ->hint('Ms')
-                                                        ->helperText('Set to zero to disable this metric.')
-                                                        ->default(0)
-                                                        ->minValue(0)
-                                                        ->numeric()
-                                                        ->required(),
-                                                ])
-                                                ->columns([
-                                                    'default' => 1,
-                                                    'md' => 2,
-                                                ]),
-                                        ]),
-                                ])
-                                ->compact()
-                                ->columns([
-                                    'default' => 1,
-                                    'md' => 2,
-                                ]),
-                        ])
-                        ->columnSpan([
-                            'md' => 2,
-                        ]),
+                                    ->compact()
+                                    ->columns([
+                                        'default' => 1,
+                                        'md' => 2,
+                                    ]),
+                            ])
+                            ->columnSpan([
+                                'md' => 2,
+                            ]),
 
-                    Card::make()
-                        ->schema([
-                            View::make('filament.forms.thresholds-helptext'),
-                        ])
-                        ->columnSpan([
-                            'md' => 1,
-                        ]),
-                ]),
-        ];
+                        Forms\Components\Section::make()
+                            ->schema([
+                                Forms\Components\View::make('filament.forms.thresholds-helptext'),
+                            ])
+                            ->columnSpan([
+                                'md' => 1,
+                            ]),
+                    ]),
+        ]);
     }
 }
