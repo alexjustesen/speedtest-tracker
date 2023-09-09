c9aea341e85b81ba622ee3e8b422cd806374b6df
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-09-09 08:26:08 -0400

    removed unused assets

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/css/filament/admin/theme.css',
-                'resources/js/app.js',
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
+            refresh: [true],
         }),
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-    },
 });
