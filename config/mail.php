e98a7ccdab54d99269fe753054c703b1e470cdc4
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-09-22 11:21:23 -0400

    [Bugfix] Fixed default mail from name variable (#807)

--- config/mail.php
+++ config/mail.php
@@ -100,7 +100,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'Speedtest Tracker'),
     ],
 
     /*
