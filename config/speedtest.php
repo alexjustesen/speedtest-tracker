27964058c90979e43e020417238f4a9b33a6b5ea
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-09-22 12:35:30 -0400

    Release v0.12.2 (#809)

--- config/speedtest.php
+++ config/speedtest.php
@@ -6,9 +6,9 @@ return [
     /**
      * Build information
      */
-    'build_date' => Carbon::parse('2023-09-19'),
+    'build_date' => Carbon::parse('2023-09-22'),
 
-    'build_version' => '0.12.1',
+    'build_version' => '0.12.2',
 
     /**
      * General
