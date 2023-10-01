95c341d5eaa903a4b1bfc0c5937a959597277993
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-10-01 17:16:41 -0400

    assets

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import presets from './vendor/filament/support/tailwind.config.preset';
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [presets],
+
+    content: [
+        './resources/views/**/*.blade.php',
+        './storage/framework/views/*.php',
+    ],
+
+    darkMode: 'class',
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        forms,
+        typography,
+    ],
+}
