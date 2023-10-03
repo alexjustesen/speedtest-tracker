4b1ca320f684c0f9653cd4491468a26ea6d3e887
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-10-03 18:49:20 -0400

    Laravel 10.26.2 Shift (#838)
    Co-authored-by: Shift <shift@laravelshift.com>

--- _ide_helper.php
+++ _ide_helper.php
@@ -4,7 +4,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.25.2.
+ * Generated for Laravel 10.26.2.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
