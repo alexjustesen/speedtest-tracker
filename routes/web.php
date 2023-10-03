daeedacf1d3a86237f3b7a60c9e9dbb3c4eb006d
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-10-03 19:44:40 -0400

    added missing route name

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', HomeController::class);
+Route::get('/', HomeController::class)
+    ->name('home');
 
 Route::get('/login', function () {
     return redirect('/admin/login');
