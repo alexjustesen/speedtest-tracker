072030c2892313a6bf7f59e0cc517ad21b954862
frknakk <5730574+frknakk@users.noreply.github.com>
2023-10-07 13:42:11 +0200

    Added public dashboard enabled setting to general settings

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Result;
+use App\Settings\GeneralSettings;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -12,6 +13,11 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $settings = new GeneralSettings();
+        if (!$settings->public_dashboard_enabled) {
+            return redirect()->route('filament.admin.auth.login');
+        }
+
         $latestResult = Result::query()
             ->select(['id', 'ping', 'download', 'upload', 'successful', 'created_at'])
             ->latest()
