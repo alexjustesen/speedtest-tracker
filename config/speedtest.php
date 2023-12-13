ad69a86133dbde67522c57fa868576ccfb9dd1d2
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-12-13 07:40:44 -0500

    bumped build information

--- config/speedtest.php
+++ config/speedtest.php
@@ -6,9 +6,9 @@ return [
     /**
      * Build information
      */
-    'build_date' => Carbon::parse('2023-12-12'),
+    'build_date' => Carbon::parse('2023-12-13'),
 
-    'build_version' => '0.14.0-beta8',
+    'build_version' => '0.14.0',
 
     /**
      * General
