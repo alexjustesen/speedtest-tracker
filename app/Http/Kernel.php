8a85b2e841cd4537deae6dbe9c5c5c667ed1ff10
Alex Justesen <alexjustesen@users.noreply.github.com>
2023-12-28 10:24:22 -0500

    removed embeddable public dashboard

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        'x-frame-allow' => \App\Http\Middleware\FrameAllowOptions::class,
     ];
 }
