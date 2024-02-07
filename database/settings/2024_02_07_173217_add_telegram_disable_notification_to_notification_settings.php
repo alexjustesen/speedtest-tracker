94587052ad71d699f9fddd181512a7f47b5e954b
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-07 12:34:26 -0500

    added Telegram disable notification setting

---
+++ database/settings/2024_02_07_173217_add_telegram_disable_notification_to_notification_settings.php
@@ -0,0 +1,11 @@
+<?php
+
+use Spatie\LaravelSettings\Migrations\SettingsMigration;
+
+return new class extends SettingsMigration
+{
+    public function up(): void
+    {
+        $this->migrator->add('notification.telegram_disable_notification', false);
+    }
+};
