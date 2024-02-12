5fd8508a26bc8212052dfc48dfba647a6cac3ce9
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-12 09:20:42 -0500

    [Feature] A better favicon (#1124)

--- resources/views/layouts/debug.blade.php
+++ resources/views/layouts/debug.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>Timezone - {{ config('app.name') }}</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
         {{-- Fonts --}}
         <link href="{{ asset('fonts/inter/inter.css') }}" rel="stylesheet" />
