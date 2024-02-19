0bab260ff3db51b8e8c9d85d1fd4dd319bbb873d
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-18 21:38:38 -0500

    prune results older than setting

--- app/Settings/GeneralSettings.php
+++ app/Settings/GeneralSettings.php
@@ -8,6 +8,8 @@ class GeneralSettings extends Settings
 {
     public bool $auth_enabled;
 
+    public int $prune_results_older_than;
+
     public ?string $speedtest_schedule;
 
     /** @var string[] */
