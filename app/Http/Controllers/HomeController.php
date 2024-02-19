7c51ef1af8a90c7c1be7828549f6eb4f0cead6cc
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-18 19:31:11 -0500

    added completed filter to dashboard query

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ResultStatus;
 use App\Models\Result;
 use App\Settings\GeneralSettings;
 use Illuminate\Http\Request;
@@ -21,6 +22,7 @@ class HomeController extends Controller
 
         $latestResult = Result::query()
             ->select(['id', 'ping', 'download', 'upload', 'status', 'created_at'])
+            ->where('status', '=', ResultStatus::Completed)
             ->latest()
             ->first();
 
