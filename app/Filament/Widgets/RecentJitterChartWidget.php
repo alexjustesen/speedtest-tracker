c5fef3f2945b13dad5444f902101367cdf6e5bf7
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-21 07:12:21 -0500

    fixed chart order by

--- app/Filament/Widgets/RecentJitterChartWidget.php
+++ app/Filament/Widgets/RecentJitterChartWidget.php
@@ -48,7 +48,7 @@ class RecentJitterChartWidget extends ChartWidget
             ->when($this->filter == 'month', function ($query) {
                 $query->where('created_at', '>=', now()->subMonth());
             })
-            ->latest()
+            ->orderBy('created_at')
             ->get();
 
         return [
