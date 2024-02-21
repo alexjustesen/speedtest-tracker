422aa82908a314b821fa0bf4af27c7b0ae35bdf0
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-20 22:40:29 -0500

    updated composer dependencies

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.44.0.
+ * Generated for Laravel 10.45.0.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
