2f9bca3368b463d3771479649a3a68ca918fdded
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-20 22:13:28 -0500

    added bit to magnitude helper

--- app/Helpers/Number.php
+++ app/Helpers/Number.php
@@ -7,6 +7,26 @@ use Illuminate\Support\Number as SupportNumber;
 class Number extends SupportNumber
 {
     /**
+     * Convert the given number to a specific bit order of magnitude.
+     */
+    public static function bitsToMagnitude(int|float $bits, int $precision = 0, string $magnitude = 'kbit'): float
+    {
+        $value = match ($magnitude) {
+            'kbit' => $bits * 1000,
+            'mbit' => $bits * pow(1000, -2),
+            'gbit' => $bits * pow(1000, -3),
+            'tbit' => $bits * pow(1000, -4),
+            'pbit' => $bits * pow(1000, -5),
+            'ebit' => $bits * pow(1000, -6),
+            'zbit' => $bits * pow(1000, -7),
+            'ybit' => $bits * pow(1000, -8),
+            default => $bits,
+        };
+
+        return static::format($value, $precision);
+    }
+
+    /**
      * Convert the given number to its largest bit order of magnitude.
      *
      * Reference: https://en.wikipedia.org/wiki/Bit
