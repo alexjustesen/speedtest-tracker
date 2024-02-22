747776f9d7e96f587cb0b1d58c781f2178e7493c
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-22 15:45:12 -0500

    [Feature] Added heathcheck API route (#1230)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\HealthCheckController;
 use App\Http\Controllers\API\Speedtest\GetLatestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/healthcheck', HealthCheckController::class);
 
 /**
  * This route provides backwards compatibility from https://github.com/henrywhitaker3/Speedtest-Tracker
