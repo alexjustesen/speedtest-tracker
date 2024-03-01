2b54ef874f22c89d692053ea8d2771d4e0d312b7
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-02-29 23:22:16 -0500

    refactored InfluxDBv2 data listener

--- app/Listeners/Data/InfluxDb2Listener.php
+++ app/Listeners/Data/InfluxDb2Listener.php
@@ -3,31 +3,20 @@
 namespace App\Listeners\Data;
 
 use App\Events\SpeedtestCompleted;
-use App\Jobs\SendDataToInfluxDbV2;
+use App\Jobs\InfluxDBv2\WriteCompletedSpeedtest;
 use App\Settings\InfluxDbSettings;
-use Illuminate\Contracts\Queue\ShouldQueue;
 
-class InfluxDb2Listener implements ShouldQueue
+class InfluxDb2Listener
 {
-    public $influxDbSettings;
-
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->influxDbSettings = new (InfluxDbSettings::class);
-    }
-
     /**
      * Handle the event.
      */
     public function handle(SpeedtestCompleted $event): void
     {
-        if ($this->influxDbSettings->v2_enabled) {
-            SendDataToInfluxDbV2::dispatch($event->result, $this->influxDbSettings);
+        $influxSettings = new InfluxDbSettings();
+
+        if ($influxSettings->v2_enabled) {
+            WriteCompletedSpeedtest::dispatch($event->result, $influxSettings);
         }
     }
 }
