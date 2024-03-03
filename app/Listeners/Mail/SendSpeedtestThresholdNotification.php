d246efdc038832b71c82a714bd8c6a8038091e4a
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-03-03 09:22:54 -0500

    filter array to prevent sending empty notifications

--- app/Listeners/Mail/SendSpeedtestThresholdNotification.php
+++ app/Listeners/Mail/SendSpeedtestThresholdNotification.php
@@ -53,6 +53,8 @@ class SendSpeedtestThresholdNotification
             array_push($failed, $this->absolutePingThreshold(event: $event, thresholdSettings: $thresholdSettings));
         }
 
+        $failed = array_filter($failed);
+
         if (! count($failed)) {
             Log::warning('Failed mail thresholds not found, won\'t send notification.');
 
@@ -68,10 +70,10 @@ class SendSpeedtestThresholdNotification
     /**
      * Build mail notification if absolute download threshold is breached.
      */
-    protected function absoluteDownloadThreshold(SpeedtestCompleted $event, ThresholdSettings $thresholdSettings): array
+    protected function absoluteDownloadThreshold(SpeedtestCompleted $event, ThresholdSettings $thresholdSettings): bool|array
     {
         if (! absoluteDownloadThresholdFailed($thresholdSettings->absolute_download, $event->result->download)) {
-            return [];
+            return false;
         }
 
         return [
@@ -84,10 +86,10 @@ class SendSpeedtestThresholdNotification
     /**
      * Build mail notification if absolute upload threshold is breached.
      */
-    protected function absoluteUploadThreshold(SpeedtestCompleted $event, ThresholdSettings $thresholdSettings): array
+    protected function absoluteUploadThreshold(SpeedtestCompleted $event, ThresholdSettings $thresholdSettings): bool|array
     {
         if (! absoluteUploadThresholdFailed($thresholdSettings->absolute_upload, $event->result->upload)) {
-            return [];
+            return false;
         }
 
         return [
@@ -100,10 +102,10 @@ class SendSpeedtestThresholdNotification
     /**
      * Build mail notification if absolute ping threshold is breached.
      */
-    protected function absolutePingThreshold(SpeedtestCompleted $event, ThresholdSettings $thresholdSettings): array
+    protected function absolutePingThreshold(SpeedtestCompleted $event, ThresholdSettings $thresholdSettings): bool|array
     {
         if (! absolutePingThresholdFailed($thresholdSettings->absolute_ping, $event->result->ping)) {
-            return [];
+            return false;
         }
 
         return [
