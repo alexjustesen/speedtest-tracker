6fa95ef036d2b2a4ba7175a2ab4edc5577479a04
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-04-09 22:05:21 -0400

    added label specifics

--- app/Enums/ResultStatus.php
+++ app/Enums/ResultStatus.php
@@ -11,17 +11,21 @@ enum ResultStatus: string implements HasColor, HasLabel
     case Failed = 'failed'; // a speedtest that failed to run successfully.
     case Started = 'started'; // a speedtest that has been started by a worker but has not finish running.
 
-    public function getLabel(): ?string
-    {
-        return $this->name;
-    }
-
     public function getColor(): ?string
     {
         return match ($this) {
             self::Completed => 'success',
             self::Failed => 'danger',
             self::Started => 'warning',
+        };
+    }
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::Completed => 'Completed',
+            self::Failed => 'Failed',
+            self::Started => 'Started',
         };
     }
 }
