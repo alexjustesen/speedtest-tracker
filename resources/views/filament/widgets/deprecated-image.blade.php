2e76515e3c447528a5728068e860df93cdda2226
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-04-15 09:18:43 -0400

    [Chore] Added deprecated image warning to admin panel (#1380)

---
+++ resources/views/filament/widgets/deprecated-image.blade.php
@@ -0,0 +1,14 @@
+<x-filament-widgets::widget>
+    <x-filament::section icon="heroicon-o-exclamation-triangle" icon-color="danger" icon-size="md">
+        <x-slot name="heading">
+            Docker Image Deprecated
+        </x-slot>
+
+        <div class="text-sm font-medium text-gray-500 dark:text-gray-400">
+            <p>
+                The Docker image you're using has been deprecated and will be replaced in <code>v0.20.0</code>. To upgrade to the new image
+                follow the steps in the <x-filament::link href="https://github.com/alexjustesen/speedtest-tracker/issues/1117" target="_blank" rel="nofollow">GitHub issue</x-filament::link> to continue to receive updates.
+            </p>
+        </div>
+    </x-filament::section>
+</x-filament-widgets::widget>
