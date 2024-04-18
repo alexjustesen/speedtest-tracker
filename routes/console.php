5271394579a1e96eafbbfae749fcdd19ee2425ac
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-04-18 10:08:46 -0400

    removed inspire from console

--- routes/console.php
+++ routes/console.php
@@ -5,13 +5,7 @@ use App\Console\Commands\SystemMaintenance;
 use App\Console\Commands\VersionChecker;
 use App\Models\Result;
 use App\Settings\GeneralSettings;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 $settings = new GeneralSettings();
 
