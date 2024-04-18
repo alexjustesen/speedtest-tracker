b1165ee609695fe2426b909ab11033e594b44009
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-04-18 09:00:22 -0400

    use /up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/healthcheck',
+        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn () => route('admin/login'));
