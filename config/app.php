02e80491c69bac86083b9b6e3f7f1a4da0829b99
Shift <shift@laravelshift.com>
2024-04-18 12:21:55 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,21 +7,6 @@ return [
 
     'force_https' => env('FORCE_HTTPS', false),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
