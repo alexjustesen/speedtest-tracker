17dd6b3efe7b24d9ae397b7c65dcfadbc8ca2300
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-04-28 07:39:47 -0400

    moved scheduled tasks to app instead of console route file to fix settings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        // commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn () => route('admin/login'));
@@ -30,4 +31,40 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        $settings = app(\App\Settings\GeneralSettings::class);
+
+        /**
+         * Checks if Result model records should be pruned.
+         */
+        if ($settings->prune_results_older_than > 0) {
+            $schedule->command('model:prune', [
+                '--model' => [\App\Models\Result::class],
+            ])->daily();
+        }
+
+        /**
+         * Perform system maintenance weekly on Sunday morning,
+         * start off the week nice and fresh.
+         */
+        $schedule->command(\App\Console\Commands\SystemMaintenance::class)->weeklyOn(0)
+            ->timezone($settings->timezone ?? 'UTC');
+
+        /**
+        * Checked for new versions weekly on Thursday because
+        * I usually do releases on Thursday or Friday.
+        */
+        $schedule->command(\App\Console\Commands\VersionChecker::class)->weeklyOn(5)
+            ->timezone($settings->timezone ?? 'UTC');
+
+        /**
+         * Action to run scheduled speedtests.
+         */
+        $schedule->call(function () {
+            \App\Actions\Speedtests\RunScheduledSpeedtests::run();
+        })
+            ->name('Run scheduled speedtests')
+            ->everyMinute()
+            ->when(! blank($settings->speedtest_schedule));
     })->create();
