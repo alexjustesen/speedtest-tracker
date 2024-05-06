e8f4f7cb4325999ae1e093a8d8048f846736d312
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-05-06 13:57:00 -0400

    added translatable title and link

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,7 +3,7 @@
         <div class="text-center">
             <h1 class="font-black text-gray-200 dark:text-gray-800 text-9xl">500</h1>
 
-            <p class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">Oops, server error!</p>
+            <p class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">{{ __('Oops, server error!') }}</p>
 
             <p class="mt-4 text-gray-500 dark:text-gray-300">There was an issue, check the logs or view the docs for help.</p>
 
@@ -12,7 +12,7 @@
                 target="_blank"
                 rel="nofollow"
                 class="inline-block px-5 py-3 mt-6 text-sm font-medium text-white rounded bg-amber-400 hover:bg-amber-500 focus:outline-none focus:ring focus:ring-amber-400/80">
-                Documentation
+                {{ __('Documentation') }}
             </a>
         </div>
     </div>
