97994e85211f9cdafb0c407c36f12df7a62d8e77
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-06-05 16:31:03 -0400

    [Feature] Refactored container startup process (BREAKING CHANGE) (#1445)

--- config/app.php
+++ config/app.php
@@ -11,7 +11,6 @@ return [
     'force_https' => env('FORCE_HTTPS', false),
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
         'TimeZoneHelper' => App\Helpers\TimeZoneHelper::class,
     ])->toArray(),
 
--- config/services.php
+++ config/services.php
@@ -3,7 +3,7 @@
 return [
 
     'telegram-bot-api' => [
-        'token' => env('TELEGRAM_BOT_TOKEN', 'YOUR BOT TOKEN HERE'),
+        'token' => env('TELEGRAM_BOT_TOKEN'),
     ],
 
 ];
