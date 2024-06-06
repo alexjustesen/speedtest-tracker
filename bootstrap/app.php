9287e826d89181acf904850b4dc493f373cbb464
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-06-06 11:35:57 -0400

    move commands to route file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Providers\AppServiceProvider;
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn () => route('admin/login'));
@@ -30,33 +30,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })
-    ->withSchedule(function (Schedule $schedule) {
-        $settings = app(\App\Settings\GeneralSettings::class);
-
-        /**
-         * Checks if Result model records should be pruned.
-         */
-        if ($settings->prune_results_older_than > 0) {
-            $schedule->command('model:prune', [
-                '--model' => [\App\Models\Result::class],
-            ])->daily();
-        }
-
-        /**
-         * Checked for new versions weekly on Thursday because
-         * I usually do releases on Thursday or Friday.
-         */
-        $schedule->command(\App\Console\Commands\VersionChecker::class)->weeklyOn(5)
-            ->timezone($settings->timezone ?? 'UTC');
-
-        /**
-         * Action to run scheduled speedtests.
-         */
-        $schedule->call(function () {
-            \App\Actions\Speedtests\RunScheduledSpeedtests::run();
-        })
-            ->name('Run scheduled speedtests')
-            ->everyMinute()
-            ->when(! blank($settings->speedtest_schedule));
     })->create();
