a8eb4b080269dd23859ff188ac0450fb78d9bf56
svenvg93 <svenvanginkel@icloud.com>
2024-06-21 23:03:29 +0200

    first_commit

---
+++ app/Actions/Notifications/SendGotifyTestNotification.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Actions\Notifications;
+
+use Filament\Notifications\Notification;
+use Lorisleiva\Actions\Concerns\AsAction;
+use Spatie\WebhookServer\WebhookCall;
+
+class SendGotifyTestNotification
+{
+    use AsAction;
+
+    public function handle(array $webhooks)
+    {
+        if (! count($webhooks)) {
+            Notification::make()
+                ->title('You need to add Gotify urls!')
+                ->warning()
+                ->send();
+
+            return;
+        }
+
+        foreach ($webhooks as $webhook) {
+            WebhookCall::create()
+                ->url($webhook['url'])
+                ->payload(['message' => '👋 Testing the Gotify notification channel.'])
+                ->doNotSign()
+                ->dispatch();
+        }
+
+        Notification::make()
+            ->title('Test Gotify notification sent.')
+            ->success()
+            ->send();
+    }
+}
