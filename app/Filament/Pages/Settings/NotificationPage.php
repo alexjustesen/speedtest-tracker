983d44740591bb74b719d7e928cddd382b71d2ca
svenvg93 <svenvanginkel@icloud.com>
2024-06-25 00:19:58 +0200

    change url

--- app/Filament/Pages/Settings/NotificationPage.php
+++ app/Filament/Pages/Settings/NotificationPage.php
@@ -152,7 +152,7 @@ class NotificationPage extends SettingsPage
                                                     ->label('Webhooks')
                                                     ->schema([
                                                         Forms\Components\TextInput::make('url')
-                                                            ->placeholder('https://push.example.com/message?token=<apptoken>')
+                                                            ->placeholder('https://example.com/message?token=<apptoken>')
                                                             ->maxLength(2000)
                                                             ->required()
                                                             ->url(),
