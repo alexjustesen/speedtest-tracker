b3c735d5c5b178175dfdf5021f8e8d8698f4cd9f
svenvg93 <svenvanginkel@icloud.com>
2024-06-25 23:45:16 +0200

    add_pushover

--- app/Settings/NotificationSettings.php
+++ app/Settings/NotificationSettings.php
@@ -46,6 +46,14 @@ class NotificationSettings extends Settings
 
     public ?array $discord_webhooks;
 
+    public bool $pushover_enabled;
+
+    public bool $pushover_on_speedtest_run;
+
+    public bool $pushover_on_threshold_failure;
+
+    public ?array $pushover_webhooks;
+
     public static function group(): string
     {
         return 'notification';
