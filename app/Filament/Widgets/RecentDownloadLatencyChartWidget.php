68389f1105d6770335eebbde53b40d4c4fb5462f
Sven van Ginkel <svenvanginkel@icloud.com>
2024-08-21 21:14:18 +0200

    Revert "Add failed and thresholds"

--- app/Filament/Widgets/RecentDownloadLatencyChartWidget.php
+++ app/Filament/Widgets/RecentDownloadLatencyChartWidget.php
@@ -5,35 +5,45 @@ namespace App\Filament\Widgets;
 use App\Enums\ResultStatus;
 use App\Models\Result;
 use Filament\Widgets\ChartWidget;
-use Filament\Widgets\Concerns\InteractsWithPageFilters;
-use Illuminate\Database\Eloquent\Builder;
 
 class RecentDownloadLatencyChartWidget extends ChartWidget
 {
-    use InteractsWithPageFilters;
-
     protected static ?string $heading = 'Download Latency';
 
     protected int|string|array $columnSpan = 'full';
 
     protected static ?string $maxHeight = '250px';
 
+    public ?string $filter = '24h';
+
     protected function getPollingInterval(): ?string
     {
         return config('speedtest.dashboard_polling');
     }
 
+    protected function getFilters(): ?array
+    {
+        return [
+            '24h' => 'Last 24h',
+            'week' => 'Last week',
+            'month' => 'Last month',
+        ];
+    }
+
     protected function getData(): array
     {
-
-        $startDate = $this->filters['startDate'] ?? now()->subWeek();
-        $endDate = $this->filters['endDate'] ?? now();
-
         $results = Result::query()
             ->select(['id', 'data', 'created_at'])
             ->where('status', '=', ResultStatus::Completed)
-            ->when($startDate, fn (Builder $query) => $query->whereDate('created_at', '>=', $startDate))
-            ->when($endDate, fn (Builder $query) => $query->whereDate('created_at', '<=', $endDate))
+            ->when($this->filter == '24h', function ($query) {
+                $query->where('created_at', '>=', now()->subDay());
+            })
+            ->when($this->filter == 'week', function ($query) {
+                $query->where('created_at', '>=', now()->subWeek());
+            })
+            ->when($this->filter == 'month', function ($query) {
+                $query->where('created_at', '>=', now()->subMonth());
+            })
             ->orderBy('created_at')
             ->get();
 
@@ -41,36 +51,33 @@ class RecentDownloadLatencyChartWidget extends ChartWidget
             'datasets' => [
                 [
                     'label' => 'Average (ms)',
-                    'data' => $averageData = $results->map(fn ($item) => $item->download_latency_iqm ? number_format($item->download_latency_iqm, 2) : 0),
-                    'borderColor' => 'rgba(16, 185, 129)',
-                    'backgroundColor' => 'rgba(16, 185, 129, 0.1)',
-                    'pointBackgroundColor' => 'rgba(16, 185, 129)',
-                    'fill' => true,
+                    'data' => $results->map(fn ($item) => $item->download_latency_iqm ? number_format($item->download_latency_iqm, 2) : 0),
+                    'borderColor' => '#10b981',
+                    'backgroundColor' => '#10b981',
+                    'pointBackgroundColor' => '#10b981',
+                    'fill' => false,
                     'cubicInterpolationMode' => 'monotone',
                     'tension' => 0.4,
-                    'pointRadius' => $averageData->count() <= 5 ? 3 : 0,
                 ],
                 [
                     'label' => 'High (ms)',
-                    'data' => $highData = $results->map(fn ($item) => $item->download_latency_high ? number_format($item->download_latency_high, 2) : 0),
-                    'borderColor' => 'rgba(14, 165, 233)',
-                    'backgroundColor' => 'rgba(14, 165, 233, 0.1)',
-                    'pointBackgroundColor' => 'rgba(14, 165, 233)',
-                    'fill' => true,
+                    'data' => $results->map(fn ($item) => $item->download_latency_high ? number_format($item->download_latency_high, 2) : 0),
+                    'borderColor' => '#0ea5e9',
+                    'backgroundColor' => '#0ea5e9',
+                    'pointBackgroundColor' => '#0ea5e9',
+                    'fill' => false,
                     'cubicInterpolationMode' => 'monotone',
                     'tension' => 0.4,
-                    'pointRadius' => $highData->count() <= 5 ? 3 : 0,
                 ],
                 [
                     'label' => 'Low (ms)',
-                    'data' => $lowData = $results->map(fn ($item) => $item->download_latency_low ? number_format($item->download_latency_low, 2) : 0),
-                    'borderColor' => 'rgba(139, 92, 246)',
-                    'backgroundColor' => 'rgba(139, 92, 246, 0.1)',
-                    'pointBackgroundColor' => 'rgba(139, 92, 246)',
-                    'fill' => true,
+                    'data' => $results->map(fn ($item) => $item->download_latency_low ? number_format($item->download_latency_low, 2) : 0),
+                    'borderColor' => '#8b5cf6',
+                    'backgroundColor' => '#8b5cf6',
+                    'pointBackgroundColor' => '#8b5cf6',
+                    'fill' => false,
                     'cubicInterpolationMode' => 'monotone',
                     'tension' => 0.4,
-                    'pointRadius' => $lowData->count() <= 5 ? 3 : 0,
                 ],
             ],
             'labels' => $results->map(fn ($item) => $item->created_at->timezone(config('app.display_timezone'))->format(config('app.chart_datetime_format'))),
@@ -82,7 +89,7 @@ class RecentDownloadLatencyChartWidget extends ChartWidget
         return [
             'scales' => [
                 'y' => [
-                    'beginAtZero' => config('app.chart_begin_at_zero'),
+                    'beginAtZero' => true,
                 ],
             ],
         ];
