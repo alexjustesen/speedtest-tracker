87158e3baffe8fc6468d24b0c97b0c29deb9e636
svenvg93 <svenvanginkel@icloud.com>
2024-09-02 15:21:50 +0200

    helptext

--- resources/views/filament/forms/latency-helptext.blade.php
+++ resources/views/filament/forms/latency-helptext.blade.php
@@ -1,7 +1,7 @@
 <div>
     <p>
-        💡 
-        Did you know that the graph is created based on the display name? 
-        You can change the URL without generating a new graph. However, changing the name will generate a new graph.
+        💡 Did you know that the graph is generated based on the display name? 
+        You can update the URL without creating a new graph, but changing the name will generate a new one. 
+        Ensure that the schedule is different from the Speedtest schedule to avoid any potential impact on the results.
     </p>
 </div>
