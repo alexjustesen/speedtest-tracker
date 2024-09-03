48843ee5a8d54b71901e3371d34fa5f0b413f694
svenvg93 <svenvanginkel@icloud.com>
2024-09-03 16:22:59 +0200

    fix_tooltip

--- app/Filament/Widgets/Latency/RecentLatencyChartWidget.php
+++ app/Filament/Widgets/Latency/RecentLatencyChartWidget.php
@@ -93,6 +93,11 @@ class RecentLatencyChartWidget extends ChartWidget
                 'legend' => [
                     'display' => true,
                 ],
+                'tooltip' => [
+                    'enabled' => true, // Enable tooltips
+                    'mode' => 'index', // Show data for all datasets at once
+                    'intersect' => false, // Don't require the mouse to intersect with a data point
+                ],
             ],
             'scales' => [
                 'y' => [
