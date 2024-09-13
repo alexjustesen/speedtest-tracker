cd141bc55029168e9dcae0c6dc94cf6a944916bc
svenvg93 <svenvanginkel@icloud.com>
2024-09-13 07:49:29 +0200

    add_migration

--- database/settings/2024_09_11_094357_create_metrics_settings.php
+++ database/settings/2024_09_11_094357_create_metrics_settings.php
@@ -6,12 +6,15 @@ class CreateMetricsSettings extends SettingsMigration
 {
     public function up(): void
     {
+        // Add Prometheus setting
         $this->migrator->add('metrics.prometheus_enabled', false);
-        $this->migrator->add('metrics.influxdb_v2_enabled', false);
-        $this->migrator->add('metrics.influxdb_v2_url', null);
-        $this->migrator->add('metrics.influxdb_v2_org', null);
-        $this->migrator->add('metrics.influxdb_v2_bucket', 'speedtest-tracker');
-        $this->migrator->add('metrics.influxdb_v2_token', null);
-        $this->migrator->add('metrics.influxdb_v2_verify_ssl', true);
+
+        // Migrate InfluxDB v2 settings from the old structure
+        $this->migrator->add('metrics.influxdb_v2_enabled', $this->migrator->get('influxdb.v2_enabled', false));
+        $this->migrator->add('metrics.influxdb_v2_url', $this->migrator->get('influxdb.v2_url', null));
+        $this->migrator->add('metrics.influxdb_v2_org', $this->migrator->get('influxdb.v2_org', null));
+        $this->migrator->add('metrics.influxdb_v2_bucket', $this->migrator->get('influxdb.v2_bucket', 'speedtest-tracker'));
+        $this->migrator->add('metrics.influxdb_v2_token', $this->migrator->get('influxdb.v2_token', null));
+        $this->migrator->add('metrics.influxdb_v2_verify_ssl', $this->migrator->get('influxdb.v2_verify_ssl', true));
     }
 }
