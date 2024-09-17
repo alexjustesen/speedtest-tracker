50f0fd1dbde58c2b0fd31f024905784757795e84
chiragkrishna <chiragkrishna@gmail.com>
2024-09-17 20:39:20 +0530

    Add-Markdown-feature-for-Gotify

--- resources/views/gotify/speedtest-threshold.blade.php
+++ resources/views/gotify/speedtest-threshold.blade.php
@@ -1,9 +1,9 @@
-Speedtest Threshold Breached - #{{ $id }}
+**Speedtest Threshold Breached - #{{ $id }}**
 
-A new speedtest was completed using {{ $service }} on {{ $isp }} but a threshold was breached.
+A new speedtest was completed using **{{ $service }}** on **{{ $isp }}** but a threshold was breached.
 
 @foreach ($metrics as $item)
-- {{ $item['name'] }} {{ $item['threshold'] }}: {{ $item['value'] }}
+- {{ $item['name'] }} {{ $item['threshold'] }}: **{{ $item['value'] }}**
 @endforeach
-- Ookla Speedtest: {{ $speedtest_url }}
-- URL: {{ $url }}
+- **Ookla Speedtest:** {{ $speedtest_url }}
+- **URL:** {{ $url }}
