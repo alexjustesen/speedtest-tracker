8c1bb58d5ae6690436f07677947ddc6e09c6823d
Sven van Ginkel <svenvanginkel@icloud.com>
2024-10-09 18:35:28 +0200

    [Bug] Remove writing server_id to an column for failed speedtests (#1679)
    * first commit
    
    * commit
    
    * Make server id optional
    
    ---------
    
    Co-authored-by: Alex Justesen <alexjustesen@users.noreply.github.com>

--- app/Jobs/Speedtests/ExecuteOoklaSpeedtest.php
+++ app/Jobs/Speedtests/ExecuteOoklaSpeedtest.php
@@ -61,11 +61,34 @@ class ExecuteOoklaSpeedtest implements ShouldBeUnique, ShouldQueue
         } catch (ProcessFailedException $exception) {
             $messages = explode(PHP_EOL, $exception->getMessage());
 
-            $message = collect(array_filter($messages, 'json_validate'))->last();
+            // Extract only the "message" part from each JSON error message
+            $errorMessages = array_map(function ($message) {
+                $decoded = json_decode($message, true);
+                if (json_last_error() === JSON_ERROR_NONE && isset($decoded['message'])) {
+                    return $decoded['message'];
+                }
 
+                return ''; // If it's not valid JSON or doesn't contain "message", return an empty string
+            }, $messages);
+
+            // Filter out empty messages and concatenate
+            $errorMessage = implode(' | ', array_filter($errorMessages));
+
+            // Prepare the error message data
+            $data = [
+                'type' => 'log',
+                'level' => 'error',
+                'message' => $errorMessage,
+            ];
+
+            // Add server ID if it exists
+            if ($this->serverId !== null) {
+                $data['server'] = ['id' => $this->serverId];
+            }
+
+            // Update the result with the error data
             $this->result->update([
-                'server_id' => $this->serverId,
-                'data' => json_decode($message, true),
+                'data' => $data,
                 'status' => ResultStatus::Failed,
             ]);
 
@@ -87,6 +110,9 @@ class ExecuteOoklaSpeedtest implements ShouldBeUnique, ShouldQueue
         SpeedtestCompleted::dispatch($this->result);
     }
 
+    /**
+     * Check for internet connection.
+     */
     protected function checkForInternetConnection(): bool
     {
         $url = config('speedtest.ping_url');
@@ -98,7 +124,6 @@ class ExecuteOoklaSpeedtest implements ShouldBeUnique, ShouldQueue
 
         if (! URL::isValidUrl($url)) {
             $this->result->update([
-                'server_id' => $this->serverId,
                 'data' => [
                     'type' => 'log',
                     'level' => 'error',
@@ -122,7 +147,6 @@ class ExecuteOoklaSpeedtest implements ShouldBeUnique, ShouldQueue
 
         if ($ping->ping() === false) {
             $this->result->update([
-                'server_id' => $this->serverId,
                 'data' => [
                     'type' => 'log',
                     'level' => 'error',
