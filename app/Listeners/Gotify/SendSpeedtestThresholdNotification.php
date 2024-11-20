cfb44ffbc7c8e3c4daed886ff5e0a5a83f37e339
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-11-20 07:15:26 -0500

    fixed array nest

--- app/Listeners/Gotify/SendSpeedtestThresholdNotification.php
+++ app/Listeners/Gotify/SendSpeedtestThresholdNotification.php
@@ -75,7 +75,6 @@ class SendSpeedtestThresholdNotification
             'extras' => [
                 'client::display' => [
                     'contentType' => 'text/markdown',
-                    ],
                 ],
             ];
 
