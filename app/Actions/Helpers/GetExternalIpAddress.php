29a797285edaa5981127a590156a487f8b71fc38
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-11-21 10:45:26 -0500

    cache external ip address response for 30s

--- app/Actions/Helpers/GetExternalIpAddress.php
+++ app/Actions/Helpers/GetExternalIpAddress.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Helpers;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
@@ -13,17 +14,21 @@ class GetExternalIpAddress
 
     public function handle(): bool|string
     {
-        $response = Http::retry(3, 100)
-            ->get('https://icanhazip.com/');
+        $externalIp = Cache::remember('external_ip', 30, function (): bool|string {
+            $response = Http::retry(3, 100)
+                ->get('https://icanhazip.com/');
 
-        if ($response->failed()) {
-            $message = sprintf('Failed to fetch public IP address, %d', $response->status());
+            if ($response->failed()) {
+                $message = sprintf('Failed to fetch public IP address, %d', $response->status());
 
-            Log::warning($message);
+                Log::warning($message);
 
-            return false;
-        }
+                return false;
+            }
 
-        return Str::trim($response->body());
+            return Str::trim($response->body());
+        });
+
+        return $externalIp;
     }
 }
