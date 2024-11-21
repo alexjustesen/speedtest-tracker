8cc01b38acca11dc30ac6b6bfa1837f9729b1af6
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-11-21 14:21:24 -0500

    removed color from result service enum

--- app/Enums/ResultService.php
+++ app/Enums/ResultService.php
@@ -2,20 +2,12 @@
 
 namespace App\Enums;
 
-use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 use Illuminate\Support\Str;
 
-enum ResultService: string implements HasColor, HasLabel
+enum ResultService: string implements HasLabel
 {
     case Ookla = 'ookla';
-
-    public function getColor(): ?string
-    {
-        return match ($this) {
-            self::Ookla => 'success',
-        };
-    }
 
     public function getLabel(): ?string
     {
