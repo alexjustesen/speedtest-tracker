767eaade58d929a48cab8a82e415d7ab514649fc
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-11-22 14:07:14 -0500

    [Bug] Fixed service enum label (#1809)
    fixed service enum label

--- app/Listeners/Gotify/SendSpeedtestThresholdNotification.php
+++ app/Listeners/Gotify/SendSpeedtestThresholdNotification.php
@@ -64,7 +64,7 @@ class SendSpeedtestThresholdNotification
         $payload = [
             'message' => view('gotify.speedtest-threshold', [
                 'id' => $event->result->id,
-                'service' => Str::title($event->result->service),
+                'service' => Str::title($event->result->service->getLabel()),
                 'serverName' => $event->result->server_name,
                 'serverId' => $event->result->server_id,
                 'isp' => $event->result->isp,
