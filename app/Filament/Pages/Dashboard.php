e16827f607f4bbd4c577ff8497b4ef24a6022d43
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-11-22 11:44:16 -0500

    replaced deprecated modal button label

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -51,11 +51,11 @@ class Dashboard extends BasePage
                 ->hidden(fn (): bool => ! config('speedtest.public_dashboard'))
                 ->url(shouldOpenInNewTab: true, url: '/'),
 
-            Action::make('ookla_speedtest')
+            Action::make('speedtest')
                 ->form([
                     Forms\Components\Select::make('server_id')
                         ->label('Select Server')
-                        ->helperText('Leave blank to run the speedtest without specifying a server.')
+                        ->helperText('Leave empty to run the speedtest without specifying a server.')
                         ->options(fn (): array => GetOoklaSpeedtestServers::run())
                         ->searchable(),
                 ])
@@ -73,8 +73,8 @@ class Dashboard extends BasePage
                         ->send();
                 })
                 ->modalHeading('Run Speedtest')
-                ->modalButton('Run Speedtest')
                 ->modalWidth('lg')
+                ->modalSubmitActionLabel('Start')
                 ->button()
                 ->color('primary')
                 ->label('Run Speedtest')
