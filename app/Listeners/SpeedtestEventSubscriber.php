8602ae075f9c82ed3ae0c0c3e331047b2053bcc0
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-12-02 19:58:13 -0500

    started work hooking into speedtest events with a subscriber

---
+++ app/Listeners/SpeedtestEventSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\SpeedtestCompleted;
+use App\Events\SpeedtestFailed;
+use Illuminate\Events\Dispatcher;
+
+class SpeedtestEventSubscriber
+{
+    /**
+     * Handle speedtest failed events.
+     */
+    public function handleSpeedtestFailed(SpeedtestFailed $event): void {}
+
+    /**
+     * Handle speedtest completed events.
+     */
+    public function handleSpeedtestCompleted(SpeedtestCompleted $event): void {}
+
+    /**
+     * Register the listeners for the subscriber.
+     */
+    public function subscribe(Dispatcher $events): void
+    {
+        $events->listen(
+            SpeedtestFailed::class,
+            [SpeedtestEventSubscriber::class, 'handleSpeedtestFailed']
+        );
+
+        $events->listen(
+            SpeedtestCompleted::class,
+            [SpeedtestEventSubscriber::class, 'handleSpeedtestCompleted']
+        );
+    }
+}
