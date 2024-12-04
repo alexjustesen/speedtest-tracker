8bdd2dfc5bf28e883beb7419e9cccafa2c7f5219
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-12-04 18:40:23 -0500

    don't cast the tags

--- app/Actions/Influxdb/v2/BuildPointData.php
+++ app/Actions/Influxdb/v2/BuildPointData.php
@@ -20,7 +20,7 @@ class BuildPointData
             ->time($result->created_at->timestamp ?? time());
 
         // Qualitative tags
-        $point->addTag('result_id', (int) $result->id)
+        $point->addTag('result_id', $result->id)
             ->addTag('external_ip', Arr::get($result->data, 'interface.externalIp'))
             ->addTag('id', $result->id)
             ->addTag('isp', Arr::get($result->data, 'isp'))
