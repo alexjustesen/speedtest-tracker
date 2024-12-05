251f87620f8b9b467b0a3e08603d241108824097
Sven van Ginkel <svenvanginkel@icloud.com>
2024-12-05 16:12:52 +0100

    Switch to on

--- resources/views/slack/speedtest-threshold.blade.php
+++ resources/views/slack/speedtest-threshold.blade.php
@@ -1,6 +1,6 @@
 **Speedtest Threshold Breached - #{{ $id }}**
 
-A new speedtest with **{{ config('app.name') }}** was completed using **{{ $service }}** on **{{ $isp }}** but a threshold was breached.
+A new speedtest on **{{ config('app.name') }}** was completed using **{{ $service }}** on **{{ $isp }}** but a threshold was breached.
 
 @foreach ($metrics as $item)
 - *{{ $item['name'] }}* {{ $item['threshold'] }}: {{ $item['value'] }}
