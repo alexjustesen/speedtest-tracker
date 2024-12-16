c62450237c1eaf314f797b6e05dce87178f8508e
Alex Justesen <alexjustesen@users.noreply.github.com>
2024-12-16 09:21:03 -0500

    Bump build date to 2024-12-16 and update version to v1.0.1

--- config/speedtest.php
+++ config/speedtest.php
@@ -4,9 +4,9 @@ use Carbon\Carbon;
 
 return [
 
-    'build_date' => Carbon::parse('2024-12-13'),
+    'build_date' => Carbon::parse('2024-12-16'),
 
-    'build_version' => 'v1.0.0',
+    'build_version' => 'v1.0.1',
 
     /**
      * General settings.
