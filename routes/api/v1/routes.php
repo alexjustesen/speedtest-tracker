6b3ebe3ee7209145acb02f7f033db972bdf14f64
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-01-13 15:30:59 -0500

    Remove commented TODOs for route protection in API v1 results routes

--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -7,14 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->name('api.v1.')->group(function () {
     Route::get('/results', ListResults::class)
-        // ->middleware() // TODO: protect the route with a gate referencing the authenticated user's role.
         ->name('results.list');
 
     Route::get('/results/latest', LatestResult::class)
-        // ->middleware() // TODO: protect the route with a gate referencing the authenticated user's role.
         ->name('results.latest');
 
     Route::get('/results/{result}', ShowResult::class)
-        // ->middleware() // TODO: protect the route with a gate referencing the authenticated user's role.
         ->name('results.show');
 });
