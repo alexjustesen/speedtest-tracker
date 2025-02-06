e435a7edc846638fb66313afdddf5d9e1739cdbc
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-02-06 18:40:50 -0500

    removed separately generating Filament theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -41,7 +41,6 @@ class AdminPanelProvider extends PanelProvider
                 provider: LocalFontProvider::class,
             )
             ->sidebarCollapsibleOnDesktop()
-            ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([])
