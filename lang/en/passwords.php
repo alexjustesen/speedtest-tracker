1bb1ad764b25344bfb116a34569c7b94d347d97c
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-18 18:42:04 -0400

    Laravel 12.x Shift (#2099)
    Co-authored-by: Shift <shift@laravelshift.com>

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -16,8 +16,5 @@ return [
     'reset' => 'Your password has been reset!',
     'sent' => 'We have emailed your password reset link!',
     'password' => 'The password and confirmation must match and contain at least six characters.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
 
 ];
