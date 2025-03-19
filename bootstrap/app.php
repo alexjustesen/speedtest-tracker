ff9714a9f12e63be3986161671a543541243be57
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-18 21:20:25 -0400

    Add GettingStarted middleware to redirect users without completed results

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\PublicDashboard;
 use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -16,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'public-dashboard' => PublicDashboard::class,
+            'getting-started' => App\Http\Middleware\GettingStarted::class,
+            'public-dashboard' => App\Http\Middleware\PublicDashboard::class,
         ]);
 
         $middleware->redirectGuestsTo(fn () => route('admin/login'));
