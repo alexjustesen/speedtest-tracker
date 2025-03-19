2c2a46bcf0e4a734f7e675602f6f493cfffca03d
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-18 20:23:40 -0400

    Add PagesController and getting started view for user onboarding

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,12 +6,18 @@ use App\Enums\ResultStatus;
 use App\Models\Result;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class PagesController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
-    public function __invoke(Request $request)
+    public function gettingStarted()
+    {
+        if (Result::where('status', '=', ResultStatus::Completed)->count()) {
+            return redirect()->route('home');
+        }
+
+        return view('getting-started');
+    }
+
+    public function home(Request $request)
     {
         $latestResult = Result::query()
             ->select(['id', 'ping', 'download', 'upload', 'status', 'created_at'])
@@ -20,7 +26,7 @@ class HomeController extends Controller
             ->first();
 
         if (! $latestResult) {
-            return view('get-started');
+            return redirect()->route('getting-started');
         }
 
         return view('dashboard', [
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('public-dashboard')->group(function () {
-    Route::get('/', HomeController::class)
+    Route::get('/', [PagesController::class, 'home'])
         ->name('home');
+
+    Route::get('/getting-started', [PagesController::class, 'gettingStarted'])
+        ->name('getting-started');
 });
 
 Route::redirect('/login', '/admin/login')
