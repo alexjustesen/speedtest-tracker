72ec532fa9c8e6add4a2908806b142bae83e07e6
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-03-18 21:20:46 -0400

    Remove unnecessary redirects in gettingStarted and home methods

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,10 +10,6 @@ class PagesController extends Controller
 {
     public function gettingStarted()
     {
-        if (Result::where('status', '=', ResultStatus::Completed)->count()) {
-            return redirect()->route('home');
-        }
-
         return view('getting-started');
     }
 
@@ -24,10 +20,6 @@ class PagesController extends Controller
             ->where('status', '=', ResultStatus::Completed)
             ->latest()
             ->first();
-
-        if (! $latestResult) {
-            return redirect()->route('getting-started');
-        }
 
         return view('dashboard', [
             'latestResult' => $latestResult,
