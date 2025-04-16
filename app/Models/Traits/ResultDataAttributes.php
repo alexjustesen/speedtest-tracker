a80cb0eaf7053b47058002fcd0e2b7aafc15d88d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-04-16 15:58:48 +0200

    feat: insert data in result

--- app/Models/Traits/ResultDataAttributes.php
+++ app/Models/Traits/ResultDataAttributes.php
@@ -19,6 +19,16 @@ trait ResultDataAttributes
     }
 
     /**
+     * Get the result's downloaded data in bytes.
+     */
+    protected function downloadedBytes(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => Arr::get($this->data, 'download.bytes'),
+        );
+    }
+
+    /**
      * Get the result's download jitter in milliseconds.
      */
     protected function downloadJitter(): Attribute
@@ -165,6 +175,16 @@ trait ResultDataAttributes
     {
         return Attribute::make(
             get: fn (): null|int|float => ! blank($this->upload) ? Bitrate::bytesToBits($this->upload) : null,
+        );
+    }
+
+    /**
+     * Get the result's uploaded data in bytes.
+     */
+    protected function uploadedBytes(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => Arr::get($this->data, 'upload.bytes'),
         );
     }
 
