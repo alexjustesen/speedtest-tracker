df1fd5c2fa9678653daf65c9ecdab9f6f2b435b7
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-04-16 18:20:53 +0200

    fix: build point data

--- app/Actions/Influxdb/v2/BuildPointData.php
+++ app/Actions/Influxdb/v2/BuildPointData.php
@@ -50,8 +50,6 @@ class BuildPointData
             ->addField('upload_latency_low', Number::castToType(Arr::get($result->data, 'upload.latency.low'), 'float'))
             ->addField('packet_loss', Number::castToType(Arr::get($result->data, 'packetLoss'), 'float'))
             ->addField('downloaded_bytes', Number::castToType(Arr::get($result->data, 'download.bytes'), 'int'))
-            ->addField('uploaded_bytes', Number::castToType(Arr::get($result->data, 'upload.bytes'), 'int'))
-            ->addField('downloaded_bytes', Number::castToType(Arr::get($result->data, 'download.bytes'), 'int'))
             ->addField('uploaded_bytes', Number::castToType(Arr::get($result->data, 'upload.bytes'), 'int'));
 
         return $point;
