8f0f3e8e9433c3a32428cc0f6d74eba5acb0a9ea
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-04-16 15:58:30 +0200

    feat: migration

---
+++ database/migrations/2025_04_16_000050_update_jobs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+         Schema::table('results', function (Blueprint $table) {
+            $table->unsignedBigInteger('downloaded_bytes')->nullable();
+            $table->unsignedBigInteger('uploaded_bytes')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('results', function (Blueprint $table) {
+            $table->dropColumn('downloaded_bytes');
+            $table->dropColumn('uploaded_bytes');
+        });
+    }
+};
