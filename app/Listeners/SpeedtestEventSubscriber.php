ebebb0f4cddfff93fa2388b13364d7a5b3474b1e
Sven van Ginkel <svenvanginkel@icloud.com>
2025-04-29 10:55:33 +0200

    update subscriber

--- app/Listeners/SpeedtestEventSubscriber.php
+++ app/Listeners/SpeedtestEventSubscriber.php
@@ -2,21 +2,16 @@
 
 namespace App\Listeners;
 
-use App\Events\SpeedtestBenchmarkFailed;
 use App\Events\SpeedtestCompleted;
 use App\Events\SpeedtestFailed;
 use App\Jobs\Influxdb\v2\WriteResult;
 use App\Jobs\Notifications\Apprise\SendSpeedtestCompletedNotification as AppriseCompleted;
-use App\Jobs\Notifications\Apprise\SendSpeedtestFailedNotification as AppriseFailed;
 use App\Jobs\Notifications\Apprise\SendSpeedtestThresholdNotification as AppriseThresholds;
 use App\Jobs\Notifications\Database\SendSpeedtestCompletedNotification as DatabaseCompleted;
-use App\Jobs\Notifications\Database\SendSpeedtestFailedNotification as DatabaseFailed;
 use App\Jobs\Notifications\Database\SendSpeedtestThresholdNotification as DatabaseThresholds;
 use App\Jobs\Notifications\Mail\SendSpeedtestCompletedNotification as MailCompleted;
-use App\Jobs\Notifications\Mail\SendSpeedtestFailedNotification as MailFailed;
 use App\Jobs\Notifications\Mail\SendSpeedtestThresholdNotification as MailThresholds;
 use App\Jobs\Notifications\Webhook\SendSpeedtestCompletedNotification as WebhookCompleted;
-use App\Jobs\Notifications\Webhook\SendSpeedtestFailedNotification as WebhookFailed;
 use App\Jobs\Notifications\Webhook\SendSpeedtestThresholdNotification as WebhookThresholds;
 use App\Settings\DataIntegrationSettings;
 use App\Settings\NotificationSettings;
@@ -29,35 +24,7 @@ class SpeedtestEventSubscriber
      */
     public function handleSpeedtestFailed(SpeedtestFailed $event): void
     {
-        $notificationSettings = app(NotificationSettings::class);
 
-        // Database notifications
-        if ($notificationSettings->database_enabled) {
-            if ($notificationSettings->database_on_speedtest_failed) {
-                DatabaseFailed::dispatch($event->result);
-            }
-        }
-
-        // Apprise notifications
-        if ($notificationSettings->apprise_enabled) {
-            if ($notificationSettings->apprise_on_speedtest_failed) {
-                AppriseFailed::dispatch($event->result);
-            }
-        }
-
-        // Webhook notifications
-        if ($notificationSettings->webhook_enabled) {
-            if ($notificationSettings->webhook_on_speedtest_failed) {
-                WebhookFailed::dispatch($event->result);
-            }
-        }
-
-        // Mail notifications
-        if ($notificationSettings->mail_enabled) {
-            if ($notificationSettings->mail_on_speedtest_failed) {
-                MailFailed::dispatch($event->result);
-            }
-        }
     }
 
     /**
@@ -149,11 +116,6 @@ class SpeedtestEventSubscriber
         $events->listen(
             SpeedtestCompleted::class,
             [SpeedtestEventSubscriber::class, 'handleSpeedtestCompleted']
-        );
-
-        $events->listen(
-            SpeedtestBenchmarkFailed::class,
-            [SpeedtestEventSubscriber::class, 'handleSpeedtestBenchmarkFailed']
         );
     }
 }
