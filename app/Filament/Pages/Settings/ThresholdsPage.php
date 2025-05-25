ae7d2eafe3e888927b64093cb5d28146d267ac6a
Sven van Ginkel <svenvanginkel@icloud.com>
2025-05-25 23:12:44 +0200

    settings pages

--- app/Filament/Pages/Settings/ThresholdsPage.php
+++ app/Filament/Pages/Settings/ThresholdsPage.php
@@ -4,8 +4,15 @@ namespace App\Filament\Pages\Settings;
 
 use App\Settings\ThresholdSettings;
 use Filament\Forms;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\View;
 use Filament\Forms\Form;
 use Filament\Pages\SettingsPage;
+use Illuminate\Support\Facades\Auth;
 
 class ThresholdsPage extends SettingsPage
 {
@@ -23,42 +30,42 @@ class ThresholdsPage extends SettingsPage
 
     public static function canAccess(): bool
     {
-        return auth()->user()->is_admin;
+        return Auth::check() && Auth::user()->is_admin;
     }
 
     public static function shouldRegisterNavigation(): bool
     {
-        return auth()->user()->is_admin;
+        return Auth::check() && Auth::user()->is_admin;
     }
 
     public function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\Grid::make([
+                Grid::make([
                     'default' => 1,
                     'md' => 3,
                 ])
                     ->schema([
-                        Forms\Components\Grid::make([
+                        Grid::make([
                             'default' => 1,
                         ])
                             ->schema([
-                                Forms\Components\Section::make('Absolute')
+                                Section::make('Absolute')
                                     ->description('Absolute thresholds do not take into account previous history and could be triggered on each test.')
                                     ->schema([
-                                        Forms\Components\Toggle::make('absolute_enabled')
+                                        Toggle::make('absolute_enabled')
                                             ->label('Enable absolute thresholds')
                                             ->reactive()
                                             ->columnSpan(2),
-                                        Forms\Components\Grid::make([
+                                        Grid::make([
                                             'default' => 1,
                                         ])
                                             ->hidden(fn (Forms\Get $get) => $get('absolute_enabled') !== true)
                                             ->schema([
-                                                Forms\Components\Fieldset::make('Metrics')
+                                                Fieldset::make('Metrics')
                                                     ->schema([
-                                                        Forms\Components\TextInput::make('absolute_download')
+                                                        TextInput::make('absolute_download')
                                                             ->label('Download')
                                                             ->hint('Mbps')
                                                             ->helperText('Set to zero to disable this metric.')
@@ -66,7 +73,7 @@ class ThresholdsPage extends SettingsPage
                                                             ->minValue(0)
                                                             ->numeric()
                                                             ->required(),
-                                                        Forms\Components\TextInput::make('absolute_upload')
+                                                        TextInput::make('absolute_upload')
                                                             ->label('Upload')
                                                             ->hint('Mbps')
                                                             ->helperText('Set to zero to disable this metric.')
@@ -74,7 +81,7 @@ class ThresholdsPage extends SettingsPage
                                                             ->minValue(0)
                                                             ->numeric()
                                                             ->required(),
-                                                        Forms\Components\TextInput::make('absolute_ping')
+                                                        TextInput::make('absolute_ping')
                                                             ->label('Ping')
                                                             ->hint('ms')
                                                             ->helperText('Set to zero to disable this metric.')
@@ -99,9 +106,9 @@ class ThresholdsPage extends SettingsPage
                                 'md' => 2,
                             ]),
 
-                        Forms\Components\Section::make()
+                        Section::make()
                             ->schema([
-                                Forms\Components\View::make('filament.forms.thresholds-helptext'),
+                                View::make('filament.forms.thresholds-helptext'),
                             ])
                             ->columnSpan([
                                 'md' => 1,
