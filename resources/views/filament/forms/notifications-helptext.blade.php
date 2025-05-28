8bea5ab77421a4cfa33a62baec8bd7ad450badba
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-05-28 05:47:44 -0700

    added notification channel deprecation notice

--- resources/views/filament/forms/notifications-helptext.blade.php
+++ resources/views/filament/forms/notifications-helptext.blade.php
@@ -1,6 +1,11 @@
 <div>
-    <p>
-        💡 Did you know, when a notification is triggered it's sent to channels.
-        Each channel can be configured separately to have the notification sent to a specific destination.
+    <h3 class="fi-section-header-heading text-base font-semibold leading-6 text-gray-950 dark:text-white">
+        ⚠️ Deprecated Channels
+    </h3>
+
+    <p class="fi-section-header-description overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400 mt-1">
+        Database, mail and webhook channels are considered core notification channels.
+
+        All other channels should be considered deprecated and will be replaced by Apprise in a future release.
     </p>
 </div>
