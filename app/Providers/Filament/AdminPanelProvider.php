825974d1c4db967e47aed97df046c518db966581
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-11-03 11:28:03 -0500

    remove custom font configuration from AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,11 +34,6 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->favicon(asset('img/speedtest-tracker-icon.png'))
-            ->font(
-                'Inter',
-                url: asset('fonts/inter/inter.css'),
-                provider: LocalFontProvider::class,
-            )
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
