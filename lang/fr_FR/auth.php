7256b4fdf71cd14db26b96fed4fd1ce204d75718
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-11-14 09:43:18 -0500

    New translations auth.php (French)

--- lang/fr_FR/auth.php
+++ lang/fr_FR/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'password' => 'Le mot de passe fourni est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
 
 ];
