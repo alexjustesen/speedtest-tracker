1840aad818015f351d6664b0b4e75d27048c9445
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-11-14 08:50:55 -0500

    New translations thresholds.php (Turkish)

---
+++ lang/tr_TR/settings/thresholds.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'title' => 'Thresholds',
+    'label' => 'Thresholds',
+
+    // Absolute thresholds
+    'absolute' => 'Absolute',
+    'absolute_description' => 'Absolute thresholds do not take into account previous history and could be triggered on each test.',
+    'absolute_enabled' => 'Enable absolute thresholds',
+
+    // Metrics section
+    'metrics' => 'Metrics',
+    'metrics_helper_text' => 'Set to zero to disable this metric.',
+
+    // General threshold labels
+    'thresholds' => 'Thresholds',
+    'threshold_enabled' => 'Threshold enabled',
+    'threshold_download' => 'Threshold download',
+    'threshold_upload' => 'Threshold upload',
+    'threshold_ping' => 'Threshold ping',
+];
