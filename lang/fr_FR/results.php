b2e4d9efe02ade3559538ae1947eaa800bbdfeb8
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-11-17 09:43:14 -0500

    New translations results.php (French)

--- lang/fr_FR/results.php
+++ lang/fr_FR/results.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'title' => 'Results',
-    'result_overview' => 'Result overview',
+    'title' => 'Résultats',
+    'result_overview' => 'Aperçu des résultats',
 
     // Metrics
-    'download_latency_high' => 'Download latency high',
-    'download_latency_low' => 'Download latency low',
+    'download_latency_high' => 'Latence de téléchargement élevée',
+    'download_latency_low' => 'Latence de téléchargement bas',
     'download_latency_iqm' => 'Download latency IQM',
     'download_latency_jitter' => 'Download latency jitter',
 
@@ -36,38 +36,38 @@ return [
 
     // Filters
     'only_healthy_speedtests' => 'Only healthy speedtests',
-    'only_unhealthy_speedtests' => 'Only unhealthy speedtests',
-    'only_manual_speedtests' => 'Only manual speedtests',
-    'only_scheduled_speedtests' => 'Only scheduled speedtests',
-    'created_from' => 'Created from',
-    'created_until' => 'Created until',
+    'only_unhealthy_speedtests' => 'Uniquement les tests de vitesse ratés',
+    'only_manual_speedtests' => 'Uniquement les tests de vitesse manuels',
+    'only_scheduled_speedtests' => 'Uniquement les tests de vitesse programmés',
+    'created_from' => 'Créé à partir de',
+    'created_until' => 'Créé jusqu\'au',
 
     // Export
-    'export_all_results' => 'Export all results',
-    'export_all_results_description' => 'Will export every column for all results.',
-    'export_completed' => 'Export completed, :count :rows exported.',
-    'failed_export' => ':count :rows failed to export.',
-    'row' => '{1} :count row|[2,*] :count rows',
+    'export_all_results' => 'Exporter tous les résultats',
+    'export_all_results_description' => 'Exporte chaque colonne pour tous les résultats.',
+    'export_completed' => 'Exportation terminée, :count :rows exportée.',
+    'failed_export' => ':count :rows a échoué à l\'exportation.',
+    'row' => '{1} :count ligne|[2,*] :count lignes',
 
     // Actions
-    'update_comments' => 'Update comments',
-    'truncate_results' => 'Truncate results',
-    'truncate_results_description' => 'Are you sure you want to truncate all results? This action is irreversible.',
-    'truncate_results_success' => 'Results table truncated!',
-    'view_on_speedtest_net' => 'View on Speedtest.net',
+    'update_comments' => 'Actualiser les commentaires',
+    'truncate_results' => 'Tronquer les résultats',
+    'truncate_results_description' => 'Êtes-vous sûr de vouloir tronquer tous les résultats ? Cette action est irréversible.',
+    'truncate_results_success' => 'Tableau des résultats tronqué !',
+    'view_on_speedtest_net' => 'Voir sur Speedtest.net',
 
     // Notifications
-    'speedtest_started' => 'Speedtest started',
-    'speedtest_completed' => 'Speedtest completed',
-    'download_threshold_breached' => 'Download threshold breached!',
-    'upload_threshold_breached' => 'Upload threshold breached!',
-    'ping_threshold_breached' => 'Ping threshold breached!',
+    'speedtest_started' => 'Test de vitesse démarré',
+    'speedtest_completed' => 'Test de vitesse terminé',
+    'download_threshold_breached' => 'Seuil de téléchargement dépassé !',
+    'upload_threshold_breached' => 'Seuil d\'envoi dépassé !',
+    'ping_threshold_breached' => 'Seuil de latence dépassé !',
 
     // Run Speedtest Action
-    'speedtest' => 'Speedtest',
-    'public_dashboard' => 'Public Dashboard',
-    'select_server' => 'Select Server',
-    'select_server_helper' => 'Leave empty to run the speedtest without specifying a server. Blocked servers will be skipped.',
-    'manual_servers' => 'Manual servers',
-    'closest_servers' => 'Closest servers',
+    'speedtest' => 'Test de vitesse',
+    'public_dashboard' => 'Tableau de bord public',
+    'select_server' => 'Sélectionner un serveur',
+    'select_server_helper' => 'Laisser vide pour exécuter le test de vitesse sans spécifier de serveur. Les serveurs bloqués seront ignorés.',
+    'manual_servers' => 'Serveurs manuels',
+    'closest_servers' => 'Serveurs les plus proches',
 ];
