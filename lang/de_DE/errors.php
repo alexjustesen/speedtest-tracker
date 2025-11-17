f4b4c7375dccd458d7311c69a81712d7fb841e24
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-11-17 11:52:30 -0500

    New translations errors.php (German)

--- lang/de_DE/errors.php
+++ lang/de_DE/errors.php
@@ -1,23 +1,23 @@
 <?php
 
 return [
-    'server_error' => 'Server Error',
-    'oops_server_error' => 'Oops, server error!',
-    'error_message' => 'Error message',
-    'error_fetching_servers' => 'Error fetching servers',
-    'servers_refreshed_successfully' => 'Servers refreshed successfully',
-    'copied_to_clipboard' => 'Copied to clipboard',
+    'server_error' => 'Serverfehler',
+    'oops_server_error' => 'Hoppla, Serverfehler!',
+    'error_message' => 'Fehlermeldung',
+    'error_fetching_servers' => 'Fehler beim Abrufen der Server',
+    'servers_refreshed_successfully' => 'Server erfolgreich aktualisiert',
+    'copied_to_clipboard' => 'In Zwischenablage kopiert',
 
     // Speedtest specific errors
-    'ookla_error' => 'An error occurred when listing speedtest servers, check the logs.',
-    'cron_invalid' => 'Invalid cron expression',
+    'ookla_error' => 'Beim Auflisten von Speedtest Servern ist ein Fehler aufgetreten. Überprüfen Sie die Logs.',
+    'cron_invalid' => 'Ungültiger Cron-Ausdruck',
 
     // Status fix command
     'status_fix' => [
-        'confirm' => 'Do you wish to continue?',
-        'fail' => 'Command aborted.',
-        'finished' => '✅ done!',
-        'info_1' => 'This will check all results and fix the status to "completed" or "failed" based on the data.',
-        'info_2' => '📖 Read the documentation: https://docs.speedtest-tracker.dev/other/commands',
+        'confirm' => 'Möchten Sie fortfahren?',
+        'fail' => 'Befehl abgebrochen.',
+        'finished' => '✅ fertig!',
+        'info_1' => 'Dies prüft alle Ergebnisse und korrigiert den Status auf "abgeschlossen" oder "fehlgeschlagen" basierend auf den Daten.',
+        'info_2' => '📖 Lesen Sie die Dokumentation: https://docs.speedtest-tracker.dev/other/commands',
     ],
 ];
