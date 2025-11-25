d1c2bac6f96258747fa6f68cb95af5d932849c35
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-11-25 14:16:20 -0600

    refactor test mail

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class Test extends Mailable implements ShouldQueue
+class TestMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
@@ -29,7 +29,7 @@ class Test extends Mailable implements ShouldQueue
     public function content(): Content
     {
         return new Content(
-            markdown: 'emails.test',
+            markdown: 'mail.test',
         );
     }
 }
