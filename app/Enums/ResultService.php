56b560e04ad2d8c580be2bfcd884da800f70db46
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-11-27 19:12:11 -0600

    scaffold run librespeed action

--- app/Enums/ResultService.php
+++ app/Enums/ResultService.php
@@ -7,12 +7,14 @@ use Filament\Support\Contracts\HasLabel;
 enum ResultService: string implements HasLabel
 {
     case Faker = 'faker';
+    case Librespeed = 'librespeed';
     case Ookla = 'ookla';
 
     public function getLabel(): ?string
     {
         return match ($this) {
             self::Faker => __('enums.service.faker'),
+            self::Librespeed => __('enums.service.librespeed'),
             self::Ookla => __('enums.service.ookla'),
         };
     }
