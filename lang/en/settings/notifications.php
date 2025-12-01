7e30ab27b4b4e278223d00dd2dea0f03a9345e28
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-11-30 18:15:54 -0600

    Update source file notifications.php

--- lang/en/settings/notifications.php
+++ lang/en/settings/notifications.php
@@ -7,28 +7,22 @@ return [
     // Database notifications
     'database' => 'Database',
     'database_description' => 'Notifications sent to this channel will show up under the 🔔 icon in the header.',
-    'database_on_speedtest_run' => 'Notify on every speedtest run',
-    'database_on_threshold_failure' => 'Notify on threshold failures',
     'test_database_channel' => 'Test database channel',
 
     // Mail notifications
     'mail' => 'Mail',
     'recipients' => 'Recipients',
-    'mail_on_speedtest_run' => 'Notify on every speedtest run',
-    'mail_on_threshold_failure' => 'Notify on threshold failures',
     'test_mail_channel' => 'Test mail channel',
 
     // Webhook
     'webhook' => 'Webhook',
     'webhooks' => 'Webhooks',
-    'webhook_on_speedtest_run' => 'Notify on every speedtest run',
-    'webhook_on_threshold_failure' => 'Notify on threshold failures',
     'test_webhook_channel' => 'Test webhook channel',
     'webhook_hint_description' => 'These are generic webhooks. For payload examples and implementation details, view the documentation.',
 
     // Common notification messages
-    'notify_on_every_speedtest_run' => 'Notify on every speedtest run',
-    'notify_on_threshold_failures' => 'Notify on threshold failures',
+    'notify_on_every_speedtest_run' => 'Notify on every scheduled speedtest run',
+    'notify_on_threshold_failures' => 'Notify on threshold failures for scheduled speedtests',
 
     // Test notification messages
     'test_notifications' => [
