ceda51736add3ac08f14521c74ee05950701b751
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-12-05 10:35:32 -0500

    feat: update admin panel dashboard layout and styling

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,10 +1,15 @@
 <x-filament-panels::page class="admin-panel-dashboard-page">
-    <div class="space-y-6">
-        <div class="grid grid-cols-1 md:grid-cols-8 gap-6">
+    <div class="space-y-6 md:space-y-12">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+            <h2 class="flex items-center gap-x-2 text-base md:text-lg font-semibold text-zinc-900 dark:text-zinc-100 col-span-full">
+                <x-lucide-bar-chart class="size-5" />
+                Platform statistics
+            </h2>
+
             {{-- <x-filament::section class="col-span-full">
                 <div class="flex items-center justify-between">
-                    <p class="text-sm/6 font-medium text-gray-500">Quota Usage</p>
-                    <a href="#" class="text-sm font-medium text-gray-600 hover:text-amber-500 underline">Edit</a>
+                    <p class="text-sm/6 font-medium text-zinc-500">Quota Usage</p>
+                    <a href="#" class="text-sm font-medium text-zinc-600 hover:text-amber-500 underline">Edit</a>
                 </div>
 
                 <div class="mt-2">
@@ -13,165 +18,176 @@
                         <span class="text-sm font-medium text-body">450MB of 1 GB</span>
                     </div>
 
-                    <div class="w-full bg-gray-200 rounded-full h-2">
+                    <div class="w-full bg-zinc-200 rounded-full h-2">
                         <div class="bg-amber-500 h-2 rounded-full" style="width: 45%"></div>
                     </div>
                 </div>
             </x-filament::section> --}}
 
             @filled($this->nextSpeedtest)
-                <x-filament::section class="col-span-2" wire:poll.60s>
-                    <p class="text-sm/6 font-medium text-gray-500">Next speedtest in</p>
+                <x-filament::section class="col-span-1" wire:poll.60s>
+                    <x-slot name="heading">
+                        Next Speedtest in
+                    </x-slot>
 
-                    <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-xl font-semibold tracking-tight text-gray-900" title="{{ $this->nextSpeedtest->format('F jS, Y g:i A') }}">{{ $this->nextSpeedtest->diffForHumans() }}</span>
-                    </p>
+                    <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100" title="{{ $this->nextSpeedtest->format('F jS, Y g:i A') }}">{{ $this->nextSpeedtest->diffForHumans() }}</p>
                 </x-filament::section>
             @else
-                <x-filament::section class="col-span-2 bg-gray-100 shadow-none">
-                    <p class="text-sm/6 font-medium text-gray-500">Next speedtest in</p>
+                <x-filament::section class="col-span-1 bg-zinc-100 shadow-none">
+                    <x-slot name="heading">
+                        Next Speedtest in
+                    </x-slot>
 
-                    <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-xl font-semibold tracking-tight text-gray-900">No scheduled speedtests</span>
-                    </p>
+                    <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">No scheduled speedtests</p>
                 </x-filament::section>
             @endfilled
 
-            <x-filament::section class="col-span-2">
-                <p class="text-sm/6 font-medium text-gray-500">Total tests</p>
+            <x-filament::section class="col-span-1">
+                <x-slot name="heading">
+                    Total tests
+                </x-slot>
 
-                <p class="mt-2 flex items-baseline gap-x-2">
-                    <span class="text-xl font-semibold tracking-tight text-gray-900">{{ $this->resultsStats['total'] }}</span>
-                </p>
+                <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->resultsStats['total'] }}</p>
             </x-filament::section>
 
-            <x-filament::section class="col-span-2">
-                <p class="text-sm/6 font-medium text-gray-500">Total successful tests</p>
+            <x-filament::section class="col-span-1">
+                <x-slot name="heading">
+                    Total successful tests
+                </x-slot>
 
-                <p class="mt-2 flex items-baseline gap-x-2">
-                    <span class="text-xl font-semibold tracking-tight text-gray-900">{{ $this->resultsStats['completed'] }}</span>
-                </p>
+                <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->resultsStats['completed'] }}</p>
             </x-filament::section>
 
-            <x-filament::section class="col-span-2">
-                <p class="text-sm/6 font-medium text-gray-500">Total failed tests</p>
+            <x-filament::section class="col-span-1">
+                <x-slot name="heading">
+                    Total failed tests
+                </x-slot>
 
-                <p class="mt-2 flex items-baseline gap-x-2">
-                    <span class="text-xl font-semibold tracking-tight text-gray-900">{{ $this->resultsStats['failed'] }}</span>
-                </p>
+                <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->resultsStats['failed'] }}</p>
             </x-filament::section>
         </div>
 
         @filled($this->latestResult)
-            <div class="w-full border-t border-gray-200"></div>
+            <div class="w-full border-t border-zinc-200 dark:border-zinc-700"></div>
 
             <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
-                <h3 class="text-base font-semibold text-gray-900 col-span-full">Latest result</h3>
+                <h2 class="flex items-center gap-x-2 text-base md:text-lg font-semibold text-zinc-900 dark:text-zinc-100 col-span-full">
+                    <x-lucide-rabbit class="size-5" />
+                    Latest result
+                </h2>
 
                 <x-filament::section class="col-span-1">
-                    <p class="text-sm/6 font-medium text-gray-500">Benchmarks</p>
+                    <x-slot name="heading">
+                        Benchmark status
+                    </x-slot>
 
-                    <div class="mt-2 flex items-center gap-x-1.5">
+                    <div class="flex items-center gap-x-2">
                         @if($this->latestResult->healthy === true)
                             <div class="flex-none rounded-full bg-emerald-500/20 p-1">
-                                <div class="size-1.5 rounded-full bg-emerald-500"></div>
+                                <div class="size-2 rounded-full bg-emerald-500"></div>
                             </div>
 
-                            <span class="text-xl font-semibold tracking-tight text-gray-900">Healthy</span>
+                            <span class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">Healthy</span>
                         @elseif($this->latestResult->healthy === false)
                             <div class="flex-none rounded-full bg-amber-500/20 p-1">
-                                <div class="size-1.5 rounded-full bg-amber-500"></div>
+                                <div class="size-2 rounded-full bg-amber-500"></div>
                             </div>
 
-                            <span class="text-xl font-semibold tracking-tight text-gray-900">Unhealthy</span>
+                            <span class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">Unhealthy</span>
                         @else
-                            <div class="flex-none rounded-full bg-gray-500/20 p-1">
-                                <div class="size-1.5 rounded-full bg-gray-500"></div>
+                            <div class="flex-none rounded-full bg-zinc-500/20 p-1">
+                                <div class="size-2 rounded-full bg-zinc-500"></div>
                             </div>
 
-                            <span class="text-xl font-semibold tracking-tight text-gray-900">Not measured</span>
+                            <span class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">Not measured</span>
                         @endif
                     </div>
                 </x-filament::section>
 
                 <x-filament::section class="col-span-1">
-                    <div class="flex items-baseline justify-between">
-                        @php
-                            $downloadBenchmark = Arr::get($this->latestResult->benchmarks, 'download');
-                        @endphp
+                    <x-slot name="heading">
+                        {{ __('general.download') }}
+                    </x-slot>
 
-                        <h4 class="text-sm/6 font-medium text-gray-500">Download</h4>
+                    @php
+                        $downloadBenchmark = Arr::get($this->latestResult->benchmarks, 'download');
+                    @endphp
 
-                        @filled($downloadBenchmark)
-                            <span class="text-xs font-medium text-gray-700 underline decoration-dotted decoration-1 decoration-gray-500 underline-offset-4" title="Benchmark: {{ true ? 'Passed' : 'Failed' }}">
+                    @filled($downloadBenchmark)
+                        <x-slot name="afterHeader">
+                            <span class="text-xs font-medium text-zinc-700 dark:text-zinc-300 underline decoration-dotted decoration-1 decoration-zinc-500 underline-offset-4" title="Benchmark: {{ true ? 'Passed' : 'Failed' }}">
                                 {{ Arr::get($downloadBenchmark, 'value').' '.str(Arr::get($downloadBenchmark, 'unit'))->title() }}
                             </span>
-                        @endfilled
-                    </div>
+                        </x-slot>
+                    @endfilled
 
-                    <p class="mt-2 flex items-baseline gap-x-2">
+                    <p class="flex items-baseline gap-x-2">
                         @php
                             $download = \App\Helpers\Bitrate::formatBits(\App\Helpers\Bitrate::bytesToBits($this->latestResult?->download));
 
                             $download = explode(' ', $download);
                         @endphp
 
-                        <span class="text-xl font-semibold tracking-tight text-gray-900">{{ $download[0] }}</span>
-                        <span class="text-sm text-gray-500">{{ $download[1].'ps' }}</span>
+                        <span class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $download[0] }}</span>
+                        <span class="text-sm text-zinc-600 dark:text-zinc-400">{{ $download[1].'ps' }}</span>
                     </p>
                 </x-filament::section>
 
                 <x-filament::section class="col-span-1">
-                    <div class="flex items-baseline justify-between">
-                        @php
-                            $uploadBenchmark = Arr::get($this->latestResult->benchmarks, 'upload');
-                        @endphp
+                    <x-slot name="heading">
+                        {{ __('general.upload') }}
+                    </x-slot>
 
-                        <h4 class="text-sm/6 font-medium text-gray-500">Upload</h4>
+                    @php
+                        $uploadBenchmark = Arr::get($this->latestResult->benchmarks, 'upload');
+                    @endphp
 
-                        @filled($uploadBenchmark)
-                            <span class="text-xs font-medium text-gray-700 underline decoration-dotted decoration-1 decoration-gray-500 underline-offset-4" title="Benchmark: {{ true ? 'Passed' : 'Failed' }}">
+                    @filled($uploadBenchmark)
+                        <x-slot name="afterHeader">
+                            <span class="text-xs font-medium text-zinc-700 dark:text-zinc-300 underline decoration-dotted decoration-1 decoration-zinc-500 underline-offset-4" title="Benchmark: {{ true ? 'Passed' : 'Failed' }}">
                                 {{ Arr::get($uploadBenchmark, 'value').' '.str(Arr::get($uploadBenchmark, 'unit'))->title() }}
                             </span>
-                        @endfilled
-                    </div>
+                        </x-slot>
+                    @endfilled
 
-                    <p class="mt-2 flex items-baseline gap-x-2">
+                    <p class="flex items-baseline gap-x-2">
                         @php
                             $upload = \App\Helpers\Bitrate::formatBits(\App\Helpers\Bitrate::bytesToBits($this->latestResult?->upload));
 
                             $upload = explode(' ', $upload);
                         @endphp
 
-                        <span class="text-xl font-semibold tracking-tight text-gray-900">{{ $upload[0] }}</span>
-                        <span class="text-sm text-gray-500">{{ $upload[1].'ps' }}</span>
+                        <span class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $upload[0] }}</span>
+                        <span class="text-sm text-zinc-600 dark:text-zinc-400">{{ $upload[1].'ps' }}</span>
                     </p>
                 </x-filament::section>
 
                 <x-filament::section class="col-span-1">
-                    <div class="flex items-baseline justify-between">
-                        @php
-                            $pingBenchmark = Arr::get($this->latestResult->benchmarks, 'ping');
-                        @endphp
+                    <x-slot name="heading">
+                        {{ __('general.ping') }}
+                    </x-slot>
 
-                        <h4 class="text-sm/6 font-medium text-gray-500">Ping</h4>
+                    @php
+                        $pingBenchmark = Arr::get($this->latestResult->benchmarks, 'ping');
+                    @endphp
 
-                        @filled($pingBenchmark)
-                            <span class="text-xs font-medium text-gray-700 underline decoration-dotted decoration-1 decoration-gray-500 underline-offset-4" title="Benchmark: {{ true ? 'Passed' : 'Failed' }}">
-                                {{ Arr::get($pingBenchmark, 'value').str(Arr::get($pingBenchmark, 'unit')) }}
+                    @filled($pingBenchmark)
+                        <x-slot name="afterHeader">
+                            <span class="text-xs font-medium text-zinc-700 dark:text-zinc-300 underline decoration-dotted decoration-1 decoration-zinc-500 underline-offset-4" title="Benchmark: {{ true ? 'Passed' : 'Failed' }}">
+                                {{ Arr::get($pingBenchmark, 'value').' '.str(Arr::get($pingBenchmark, 'unit')) }}
                             </span>
-                        @endfilled
-                    </div>
+                        </x-slot>
+                    @endfilled
 
-                    <p class="mt-2 flex items-baseline gap-x-2">
-                        <span class="text-xl font-semibold tracking-tight text-gray-900">{{ $this->latestResult?->ping }}</span>
-                        <span class="text-sm text-gray-500">ms</span>
+                    <p class="flex items-baseline gap-x-2">
+                        <span class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->latestResult?->ping }}</span>
+                        <span class="text-sm text-zinc-600 dark:text-zinc-400">ms</span>
                     </p>
                 </x-filament::section>
             </div>
         @endfilled
 
-        <div class="w-full border-t border-gray-200"></div>
+        <div class="w-full border-t border-zinc-200 dark:border-zinc-700"></div>
 
         <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
             <x-filament::section class="col-span-1" icon="lucide-book-open-text">
@@ -179,7 +195,7 @@
                     {{ __('general.documentation') }}
                 </x-slot>
 
-                <div class="text-sm text-gray-600 dark:text-gray-300">
+                <div class="text-sm text-zinc-600 dark:text-zinc-300">
                     <p>Need help getting started or configuring your speedtests?</p>
                 </div>
 
@@ -199,7 +215,7 @@
                     {{ __('general.donations') }}
                 </x-slot>
 
-                <div class="text-sm text-gray-600 dark:text-gray-300">
+                <div class="text-sm text-zinc-600 dark:text-zinc-300">
                     <p>Support the development and maintenance of Speedtest Tracker by making a donation.</p>
                 </div>
 
@@ -230,7 +246,7 @@
                     </x-slot>
                 @endif
 
-                <ul role="list" class="divide-y divide-gray-200 space-y-2 text-sm text-gray-600 dark:text-gray-300">
+                <ul role="list" class="divide-y divide-zinc-200 space-y-2 text-sm text-zinc-600 dark:text-zinc-300">
                     <li class="flex items-center justify-between pb-2">
                         <p class="font-medium">{{ __('general.current_version') }}</p>
                         <p>{{ config('speedtest.build_version') }}</p>
