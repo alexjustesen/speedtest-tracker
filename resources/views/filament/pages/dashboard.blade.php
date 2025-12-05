a47e3225e5a3e4346d2b401c5991ad5ca55fa87f
Alex Justesen <alexjustesen@users.noreply.github.com>
2025-12-05 15:33:44 -0500

    Release v1.12.0 (#2493)
    Co-authored-by: Sven van Ginkel <svenvanginkel@icloud.com>
    Co-authored-by: Alex Justesen <1144087+alexjustesen@users.noreply.github.com>

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,3 +1,97 @@
-<x-filament-panels::page>
-    {{-- Silence is golden --}}
+<x-filament-panels::page class="dashboard-page">
+    <div class="space-y-6 md:space-y-12">
+        <livewire:platform-stats />
+
+        <livewire:latest-result-stats />
+
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <x-filament::section
+                class="col-span-1"
+                icon="tabler-book"
+                icon-size="md"
+            >
+                <x-slot name="heading">
+                    {{ __('general.documentation') }}
+                </x-slot>
+
+                <div class="text-sm text-zinc-600 dark:text-zinc-300">
+                    <p>Need help getting started or configuring your speedtests?</p>
+                </div>
+
+                <div class="mt-5">
+                    <x-filament::button
+                        href="https://docs.speedtest-tracker.dev?utm_source=app&utm_medium=dashboard&utm_campaign=view_documentation"
+                        tag="a"
+                        target="_blank"
+                    >
+                        {{ __('general.view_documentation') }}
+                    </x-filament::button>
+                </div>
+            </x-filament::section>
+
+            <x-filament::section
+                class="col-span-1"
+                icon="tabler-cash-banknote-heart"
+                icon-size="md"
+            >
+                <x-slot name="heading">
+                    {{ __('general.donations') }}
+                </x-slot>
+
+                <div class="text-sm text-zinc-600 dark:text-zinc-300">
+                    <p>Support the development and maintenance of Speedtest Tracker by making a donation.</p>
+                </div>
+
+                <div class="mt-5">
+                    <x-filament::button
+                        href="https://github.com/sponsors/alexjustesen?utm_source=app&utm_medium=dashboard&utm_campaign=donate"
+                        tag="a"
+                        target="_blank"
+                    >
+                        {{ __('general.donate') }}
+                    </x-filament::button>
+                </div>
+            </x-filament::section>
+
+            <x-filament::section
+                class="col-span-1"
+                icon="tabler-brand-github"
+                icon-size="md"
+            >
+                <x-slot name="heading">
+                    {{ __('general.speedtest_tracker') }}
+                </x-slot>
+
+                @if (\App\Services\GitHub\Repository::updateAvailable())
+                    <x-slot name="afterHeader">
+                        <x-filament::badge>
+                            {{ __('general.update_available') }}
+                        </x-filament::badge>
+                    </x-slot>
+                @endif
+
+                <ul role="list" class="divide-y divide-zinc-200 space-y-2 text-sm text-zinc-600 dark:text-zinc-300">
+                    <li class="flex items-center justify-between pb-2">
+                        <p class="font-medium">{{ __('general.current_version') }}</p>
+                        <p>{{ config('speedtest.build_version') }}</p>
+                    </li>
+
+                    <li class="flex items-center justify-between">
+                        <p class="font-medium">{{ __('general.latest_version') }}</p>
+                        <p>{{ \App\Services\GitHub\Repository::getLatestVersion() }}</p>
+                    </li>
+                </ul>
+
+                <div class="mt-5">
+                    <x-filament::button
+                        href="https://github.com/alexjustesen/speedtest-tracker?utm_source=app&utm_medium=dashboard&utm_campaign=github"
+                        tag="a"
+                        target="_blank"
+                    >
+                        {{ __('general.github') }} {{ str(__('general.repository'))->lower() }}
+                    </x-filament::button>
+                </div>
+            </x-filament::section>
+        </div>
+    </div>
 </x-filament-panels::page>
---
+++ resources/views/livewire/platform-stats.blade.php
@@ -0,0 +1,68 @@
+<div wire:poll.60s>
+    <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <h2 class="flex items-center gap-x-2 text-base md:text-lg font-semibold text-zinc-900 dark:text-zinc-100 col-span-full">
+            <x-tabler-chart-bar class="size-5" />
+            {{ __('general.statistics') }}
+        </h2>
+
+        {{-- <x-filament::section class="col-span-full">
+            <div class="flex items-center justify-between">
+                <p class="text-sm/6 font-medium text-zinc-500">Quota Usage</p>
+                <a href="#" class="text-sm font-medium text-zinc-600 hover:text-amber-500 underline">Edit</a>
+            </div>
+
+            <div class="mt-2">
+                <div class="flex justify-between mb-1">
+                    <span class="text-sm font-medium text-body">Bandwidth</span>
+                    <span class="text-sm font-medium text-body">450MB of 1 GB</span>
+                </div>
+
+                <div class="w-full bg-zinc-200 rounded-full h-2">
+                    <div class="bg-amber-500 h-2 rounded-full" style="width: 45%"></div>
+                </div>
+            </div>
+        </x-filament::section> --}}
+
+        @filled($this->nextSpeedtest)
+            <x-filament::section class="col-span-1">
+                <x-slot name="heading">
+                    Next Speedtest in
+                </x-slot>
+
+                <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100" title="{{ $this->nextSpeedtest->format('F jS, Y g:i A') }}">{{ $this->nextSpeedtest->diffForHumans() }}</p>
+            </x-filament::section>
+        @else
+            <x-filament::section class="col-span-1 bg-zinc-100 shadow-none">
+                <x-slot name="heading">
+                    Next Speedtest in
+                </x-slot>
+
+                <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">No scheduled speedtests</p>
+            </x-filament::section>
+        @endfilled
+
+        <x-filament::section class="col-span-1">
+            <x-slot name="heading">
+                Total tests
+            </x-slot>
+
+            <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->platformStats['total'] }}</p>
+        </x-filament::section>
+
+        <x-filament::section class="col-span-1">
+            <x-slot name="heading">
+                Total successful tests
+            </x-slot>
+
+            <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->platformStats['completed'] }}</p>
+        </x-filament::section>
+
+        <x-filament::section class="col-span-1">
+            <x-slot name="heading">
+                Total failed tests
+            </x-slot>
+
+            <p class="text-xl font-semibold tracking-tight text-zinc-900 dark:text-zinc-100">{{ $this->platformStats['failed'] }}</p>
+        </x-filament::section>
+    </div>
+</div>
