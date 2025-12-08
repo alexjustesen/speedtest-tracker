7cfb487185241d579dfe8e45896e319183a8b562
sveng93 <svenvanginkel@icloud.com>
2025-12-08 22:47:48 +0100

    remove seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,9 +18,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        $this->call([
-            ResultSeeder::class,
-        ]);
     }
 }
