2864ad59280d939693977032ae262aab06b70c8a
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-12-08 20:17:14 -0500

    feat: move results table outside the admin panel for improved layout

--- resources/views/dashboard/results.blade.php
+++ resources/views/dashboard/results.blade.php
@@ -1,3 +1,16 @@
 <x-app-layout title="Results">
-    <livewire:list-results />
+    <div class="space-y-6 md:space-y-12 dashboard-page">
+        <livewire:next-speedtest-banner />
+
+        <livewire:latest-result-stats />
+
+        <div class="grid grid-cols-1 gap-6">
+            <h2 class="flex items-center gap-x-2 text-base md:text-lg font-semibold text-zinc-900 dark:text-zinc-100 col-span-full">
+                <x-tabler-table class="size-5" />
+                Results
+            </h2>
+
+            <livewire:list-results />
+        </div>
+    </div>
 </x-app-layout>
