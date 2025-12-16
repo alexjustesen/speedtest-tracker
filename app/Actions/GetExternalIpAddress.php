ef539546608aeca8dbb5b2cfabba675dc56aa6c8
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-12-16 07:28:02 -0500

    refactor: simplify error handling in GetExternalIpAddress action

--- app/Actions/GetExternalIpAddress.php
+++ app/Actions/GetExternalIpAddress.php
@@ -2,12 +2,11 @@
 
 namespace App\Actions;
 
-use Illuminate\Http\Client\RequestException;
-use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Lorisleiva\Actions\Concerns\AsAction;
+use Throwable;
 
 class GetExternalIpAddress
 {
@@ -17,14 +16,15 @@ class GetExternalIpAddress
     {
         $address = $address ?? config('speedtest.preflight.get_external_ip_url');
 
-        $response = Http::retry(3, 100)
-            ->timeout(5)
-            ->throw(function (Response $response, RequestException $e) use ($address) {
-                Log::error('Failed to fetch external IP address from "'.$address.'".', [$e->getMessage()]);
+        try {
+            $response = Http::retry(3, 100)
+                ->timeout(5)
+                ->get(url: $address);
+        } catch (Throwable $th) {
+            Log::error('Failed to fetch external IP address from "'.$address.'".', [$th->getMessage()]);
 
-                return false;
-            })
-            ->get(url: $address);
+            return false;
+        }
 
         return Str::trim($response->body());
     }
