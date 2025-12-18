b7b4488f7e4944df4ccd5c9fb483b11067f90fb3
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2025-12-18 16:05:35 -0500

    revert change to next speedtest banner

--- resources/views/livewire/next-speedtest-banner.blade.php
+++ resources/views/livewire/next-speedtest-banner.blade.php
@@ -1,5 +1,17 @@
 <div wire:poll.60s>
     @if ($this->nextSpeedtest)
-        <flux:callout color="blue" icon="information-circle" heading="Next speedtest scheduled at {{ $this->nextSpeedtest->timezone(config('app.display_timezone'))->format(config('app.datetime_format')) }}" />
+        <div class="rounded-md bg-blue-50 dark:bg-blue-500/10 p-4 outline outline-blue-500/20">
+            <div class="flex">
+                <div class="shrink-0">
+                    <x-tabler-info-circle class="size-5 text-blue-400" />
+                </div>
+
+                <div class="ml-3 flex-1">
+                    <p class="text-sm text-blue-700 dark:text-blue-300">
+                        Next scheduled test at <span class="font-medium">{{ $this->nextSpeedtest->timezone(config('app.display_timezone'))->format(config('app.datetime_format')) }}</span>.
+                    </p>
+                </div>
+            </div>
+        </div>
     @endif
 </div>
