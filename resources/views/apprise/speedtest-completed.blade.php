a548dfd63ff271528eccff16020d2e08d1c2a107
sveng93 <svenvanginkel@icloud.com>
2026-02-03 14:58:48 +0100

    chore: use empty string and update apprise template

--- resources/views/apprise/speedtest-completed.blade.php
+++ resources/views/apprise/speedtest-completed.blade.php
@@ -6,7 +6,9 @@ A new speedtest on **{{ config('app.name') }}** was completed using **{{ $servic
 - **Ping:** {{ $ping }}
 - **Download:** {{ $download }}
 - **Upload:** {{ $upload }}
+@if($packetLoss)
 - **Packet Loss:** {{ $packetLoss }}%
+@endif
 
 ### Links
 - [View Ookla Results]({{ $speedtest_url }})
