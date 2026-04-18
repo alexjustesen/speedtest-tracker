1e6af80e362d9c9aecb7365c78efee9783492568
Alex Justesen <1144087+alexjustesen@users.noreply.github.com>
2026-04-18 10:14:44 -0400

    upgraded to Laravel v13

--- config/sanctum.php
+++ config/sanctum.php
@@ -77,7 +77,7 @@ return [
     'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class,
     ],
 
 ];
