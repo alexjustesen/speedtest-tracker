f886f0fbf2477c582b304a6f06a0699fd8db41a0
Alex Justesen <alexjustesen@users.noreply.github.com>
2026-07-23 09:37:43 -0400

    release: v1.14.6 (#2846)
    Co-authored-by: Alex Justesen <1144087+alexjustesen@users.noreply.github.com>

--- config/speedtest.php
+++ config/speedtest.php
@@ -6,9 +6,9 @@ return [
     /**
      * General settings.
      */
-    'build_date' => Carbon::parse('2026-06-22'),
+    'build_date' => Carbon::parse('2026-07-23'),
 
-    'build_version' => 'v1.14.5',
+    'build_version' => 'v1.14.6',
 
     'content_width' => env('CONTENT_WIDTH', '7xl'),
 
