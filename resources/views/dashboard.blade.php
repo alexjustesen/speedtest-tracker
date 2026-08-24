40dd542d2e661ecc715f78126bcf46567252190f
sveng93 <svenvanginkel@icloud.com>
2026-08-24 08:11:09 +0200

    Revert StatsOverviewWidget

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
     <div class="space-y-6 md:space-y-12 dashboard-page">
         <livewire:next-speedtest-banner />
 
-        @livewire(\App\Filament\Widgets\StatsOverviewWidget::class)
+        <livewire:latest-result-stats />
 
         <div class="grid grid-cols-1 gap-6">
             <h2 class="flex items-center gap-x-2 text-base md:text-lg font-semibold text-zinc-900 dark:text-zinc-100 col-span-full">
