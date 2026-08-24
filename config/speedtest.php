2f1f26e52ebc3d25cdb49ced0bc83e3622cd1a52
sveng93 <svenvanginkel@icloud.com>
2026-08-24 08:45:53 +0200

    remove dead config option

--- config/speedtest.php
+++ config/speedtest.php
@@ -16,8 +16,6 @@ return [
 
     'public_dashboard' => env('PUBLIC_DASHBOARD', false),
 
-    'default_chart_range' => strtolower(env('DEFAULT_CHART_RANGE', '24h')),
-
     'chart_only_show_avg_latency' => (bool) env('CHART_ONLY_SHOW_AVG_LATENCY', false),
 
     /**
